6c33278a3897a75b79b8dd78f2d9adb8cc1728a2
ponomarev-vitaly <vp211082@gmail.com>
2023-02-09 05:12:18 +0300

    Initial commit: I created SDET Project and Hello World class.

---
+++ src/week1/HelloWorld.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+        System.out.print("Welcome to Clarusway SDET Course: ");
+        System.out.println("This line is starting with the previous line");
+        System.out.println("Again we are starting from new line");
+    }
+}
