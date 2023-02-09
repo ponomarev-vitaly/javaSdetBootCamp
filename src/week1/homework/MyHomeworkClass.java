b7ea45bf381085dd5744551442169d3113b7051c
ponomarev-vitaly <vp211082@gmail.com>
2023-02-10 01:03:04 +0300

    I made the homework for the 09.02.2023. Lines of code from 93 to 132 need to be checked as I got an Error: IDE reached the end of the file while parcing.

---
+++ src/week1/homework/MyHomeworkClass.java
@@ -0,0 +1,137 @@
+package week1.homework;
+
+public class MyHomeworkClass {
+    public static void main(String[] args) {
+        System.out.println("Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.");
+    // This is single line comment
+
+    /*
+    This is regular multi-line comment
+    * */
+
+    /**
+    * This is Java Doc comment and it appears in green color.
+    */
+
+    // Class Name: MyHomeworkClass
+    // Method Name: main()
+    // Developer: <Vitalii Ponomarev>
+    // Date: November 23,2022
+
+    /*
+    Class Name: MyHomeworkClass
+	Method Name: main()
+	Developer: <Vitalii Ponomarev>
+	Date: November 23,2022
+     */
+
+    /**
+     * Class Name: MyHomeworkClass
+     * Method Name: main()
+     * Developer: <Vitalii Ponomarev>
+     * Date: November 23,2022
+    */
+    }
+
+    public class Student {
+        private String name; // * The first and last name of this student.
+        public Student(String name) {
+            this.name = name;
+        }
+        // Creates a new Student with the given name. The name should include both first and last name.
+    }
+
+    // Create a new class and main method with any name and copy below codes and correct the wrong comments. Java program to show single line comments.
+
+    class SingleComments
+    {
+        public static void main(String args[])
+        {
+            // Single line comment here
+            System.out.println("Single line comment above");
+        }
+    }
+
+    // Create a new class and main method with any name and copy below codes and correct the wrong comments. Java program to show multi line comments.
+    class SingleComment
+    {
+        public static void main(String args[])
+        {
+            System.out.println("Multi line comments below");
+            /**
+             * Comment line 1
+             * Comment line 2
+             * Comment line 3
+             * */
+        }
+    }
+
+    // Create a new class and main method with any name and copy below codes and correct the wrong comments.
+    /**Comment start
+     *
+     *tags are used in order to specify a parameter
+     *or method or heading
+     *HTML tags can also be used
+     *such as <h1>
+     *
+     *comment ends*/
+
+    // package whatever; //do not write package name here
+
+    // import java.io.*;
+
+    public class MyClass {
+        public static void main (String[] args) {
+
+            // comment line 1
+            // comment line 2
+
+        }
+    }
+}
+
+//    // Create a new class and main method with any name and copy below codes and correct the wrong comments.
+//     */
+//    // Java program to illustrate frequently used comment tags.
+//    /* * <h1>Find average of three numbers!</h1>
+//     * The FindAvg program implements an application that
+//     * simply calculates average of three integers and Prints
+//     * the output on the screen.
+//     *
+//     * @author John Doe
+//     * @version 1.0
+//     * @since 11/23/2022
+//     */
+//
+//    public class FindAvg {
+//
+//	/* This method is used to find average of three integers.
+//    * @param numA This is the first parameter to findAvg method
+//	* @param numB This is the second parameter to findAvg method
+//	* @param numC This is the second parameter to findAvg method
+//	* @return int This returns average of numA, numB and numC.
+//	*/
+//
+//        public int findAvg(int numA, int numB, int numC) {
+//            return (numA + numB + numC)/3;
+//        }
+//
+//
+//	/* This is the main method which makes use of findAvg method.
+//	@param args Unused.
+//	@return Nothing.
+//	*/
+//
+//
+//        public static void main(String args[]) {
+//            FindAvg obj = new FindAvg();
+//            int avg = obj.findAvg(10, 20, 30);
+//
+//            System.out.println("Average of 10, 20 and 30 is :" + avg);
+//        }
+//    }
+
+
+
+
+
