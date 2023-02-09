a408347bbf0068f0613b16e8bd908145e25a61b8
ponomarev-vitaly <vp211082@gmail.com>
2023-02-09 21:27:31 +0300

    I added one file to check the Java Arithmetic Operators.

---
+++ src/week1/ArithmeticOperators.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int total = 20 + 5; // total is 25
+        System.out.println(total);
+        int num1 = 30;
+        int num2 = 20;
+        int total1 = 10 + num2; // total is 30
+        System.out.println(total1);
+        int total2 = num1 + num2; // total is 50
+        System.out.println(total2);
+
+    }
+}
