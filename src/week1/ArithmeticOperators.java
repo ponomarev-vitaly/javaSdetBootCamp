e1c65334a6c0a30db1527539b390a59fbf1c7a04
ponomarev-vitaly <vp211082@gmail.com>
2023-02-09 21:52:03 +0300

    I added more operators in the file to check the Java Arithmetic Operators.

--- src/week1/ArithmeticOperators.java
+++ src/week1/ArithmeticOperators.java
@@ -10,6 +10,26 @@ public class ArithmeticOperators {
         System.out.println(total1);
         int total2 = num1 + num2; // total is 50
         System.out.println(total2);
+        // let's have a look at subtraction
+        int result = 20 - 5; //result is 15
+        int num3 = 30;
+        int num4 = 20;
+        int result1 = 40 - num4; //result is 20
+        int result2 = num3 - num4; //result is 10 now
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        // let's have a look at %Modulus, Increment, Decrement
+        int mod = 20 % 3;
+        // --> 20/3 = 6 (2 remainder)
+        // --> mod = 2
+        System.out.println(mod);
+        int randomNumber = 1;
+        int num5 = ++randomNumber;
+        System.out.println(num5);
+        int num6 = randomNumber++;
+        System.out.println(num6); // It seems there is no difference between the ++ before the variable and after.
+
 
     }
 }
