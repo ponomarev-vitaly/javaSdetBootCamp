c2fb2062bab5b0c22708514776893106d6990e31
ponomarev-vitaly <vp211082@gmail.com>
2023-02-09 07:11:12 +0300

    I added Primitive Data Type class.

---
+++ src/week1/PrimitiveDataType.java
@@ -0,0 +1,20 @@
+package week1;
+
+public class PrimitiveDataType {
+    boolean isCold = true;
+    boolean isHeavy = false;
+
+    char initial = 'C';
+
+    byte age = 22;
+
+    short salary = 1000;
+
+    System.out.println(isCold);
+    System.out.println(isHeavy);
+    System.out.println(initial);
+    System.out.println(age);
+    System.out.println(salary);
+
+    }
+}
