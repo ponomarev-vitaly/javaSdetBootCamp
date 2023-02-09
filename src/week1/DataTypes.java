6e7328073d9d389ad0b45d869dacc741dfe0c8ca
ponomarev-vitaly <vp211082@gmail.com>
2023-02-09 06:38:25 +0300

    I added DataTypes class.

---
+++ src/week1/DataTypes.java
@@ -0,0 +1,27 @@
+package week1;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // Variables declaration as below:
+        int age;
+        char initial;
+
+        // Assigning Values to variables as below:
+        age = 25;
+        initial = 'M';
+
+        int age1, height, distance;
+        age1 = 15;
+        height = 150;
+        distance = 75;
+
+        int age2 = 11, height1 = 120, distance1 = 100;
+
+        System.out.println(age1);
+        System.out.println(height1);
+        System.out.println(distance1);
+
+        System.out.println(initial);
+
+    }
+}
