1b283f302e5bb0f4d9e823bd38f4be48b02a8e14
ponomarev-vitaly <vp211082@gmail.com>
2023-02-09 20:54:58 +0300

    I added one file from the test to check the code.

---
+++ src/week1/Example1.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class Example1 {
+    public class Utility {
+        public static void main(String[] args) {
+            System.out.println("Clarusway!");
+        }
+    }
+}
