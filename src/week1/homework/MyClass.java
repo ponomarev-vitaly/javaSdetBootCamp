10f1eb010e6ef3088d2550feb14a2dca165546e9
ponomarev-vitaly <vp211082@gmail.com>
2023-02-10 03:58:18 +0300

    I created a new class and main method with any name and copy below codes and correct the wrong comments in an example N 7.

---
+++ src/week1/homework/MyClass.java
@@ -0,0 +1,24 @@
+package week1.homework;
+/**Comment start
+ *
+ *tags are used in order to specify a parameter
+ *or method or heading
+ *HTML tags can also be used
+ *such as <h1>
+ *
+ *comment ends*/
+public class MyClass {
+
+package week1.homework; //do not write package name here
+
+import java.io.*;
+
+    class MyClass {
+        public static void main (String[] args) {
+
+            /** comment line 1
+            comment line 2 */
+
+        }
+    }
+}
