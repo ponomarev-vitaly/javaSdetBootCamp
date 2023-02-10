4460764b3b1310e004a7fe020d11b5e2aa670812
ponomarev-vitaly <vp211082@gmail.com>
2023-02-10 06:34:33 +0300

    I added a file with several examples of the Escape Sequences.

---
+++ src/week1/EscapeSequences.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class EscapeSequences {
+    public static void main(String[] args) {
+        /**
+         * Escape character     Result  Description
+         * \'                   '       Single quote
+         * \"                   "       Double quote
+         * \\                   \       Backslash
+         */
+    }
+}
