b0308c7a0995b061c9c366fcd4f9ce8bb977f549
ponomarev-vitaly <vp211082@gmail.com>
2023-02-10 05:41:23 +0300

    I added methods for the Non Primitive Data Types of the JAVA variables.

--- src/week1/NonPrimitiveDataType.java
+++ src/week1/NonPrimitiveDataType.java
@@ -4,7 +4,16 @@ public class NonPrimitiveDataType {
     public static void main(String[] args) {
         String name = "Adam";
         System.out.println("NAME : " + name);
-        System.out.println("Name variables length is " + name.length() + ".");
+        System.out.println("Name variable length is " + name.length() + ".");
+        int nameLength = name.length();
+        System.out.println("Name's length : " + nameLength);
+
+        String txt = "Clarusway SDET Course.";
+        System.out.println(txt);
+        System.out.println(txt.toUpperCase());
+        System.out.print(txt.toLowerCase());
+
+        System.out.println(txt.indexOf("s"));
 
     }
 }
