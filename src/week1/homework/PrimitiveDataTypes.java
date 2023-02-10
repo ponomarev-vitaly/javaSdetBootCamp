1668f825d3de6d7bea88c726d86d1ae87dccb61d
ponomarev-vitaly <vp211082@gmail.com>
2023-02-11 00:29:47 +0300

    I did all the exercises with the Primitive Data Types.

--- src/week1/homework/PrimitiveDataTypes.java
+++ src/week1/homework/PrimitiveDataTypes.java
@@ -1,4 +1,91 @@
 package week1.homework;
+/*
+ 1-) Create a class and main method and print the values:
 
+    byte:
+    create a byte variable name called maxRange and assign the value of 127
+    print the variable called maxRange.
+    create a byte variable name called minRange and assign the value of -128
+    print the variable called minRange.
+    create a byte variable name called anyRange and assign the value of 101
+    print the variable called anyRange.
+    create a byte variable name called randomRange and assign the value of 1
+    print the variable called randomRange.
+
+    short:
+    create a short variable name called maxShortRange and assign the value of 32767
+    print the variable called maxShortRange.
+    create a short variable name called minshortRange and assign the value of -32768
+    print the variable called minRange.
+    create a short variable name called anyshortRange and assign the value of 20000
+    print the variable called anyRange.
+    create a short variable name called randomShortRange and assign the value of -12758
+    print the variable called randomRange.
+
+    int:
+    create a int variable name called maxInttRange and assign the value of 2_147_483_647
+    print the variable called maxIntRange.
+    create a int variable name called minIntRange and assign the value of -2_147_483_648
+    print the variable called minIntRange.
+    create a int variable name called anyIntRange and assign the value of 1234566
+    print the variable called anyIntRange.
+    create a int variable name called randomIntRange and assign the value of -3_456_798
+    print the variable called randomIntRange.
+
+    long: (don't forget to put the capital L at the end of each number)
+    create a long variable name called maxLongtRange and assign the value of 9_223_372_036_854_775_808
+    print the variable called maxLongtRange.
+    create a long variable name called minLongRange and assign the value of -9_223_372_036_854_775_807
+    print the variable called minLongRange.
+    create a long variable name called anyLongRange and assign the value of 4_123_456_678_922
+    print the variable called anyLongRange.
+    create a long variable name called randomLongRange and assign the value of -3_456_798_999_404
+    print the variable called randomLongRange.
+
+    boolean :
+    create a boolean variable name called myBooleanValue and assign the value of true
+    print the variable called myBooleanValue.
+    create a boolean variable name called iAmGoingToBe and assign the value of true
+    print the variable called myBooleanValue.
+ */
 public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte maxRange = 127; // This block of code is for the byte variables
+        System.out.println(maxRange);
+        byte minRange = -128;
+        System.out.println(minRange);
+        byte anyRange = 101;
+        System.out.println(anyRange);
+        byte randomRange = 1;
+        System.out.println(randomRange);
+        short maxShortRange = 32767; //This block of code is for the short variables.
+        System.out.println(maxShortRange);
+        short minshortRange = -32768;
+        System.out.println(minshortRange);
+        short anyshortRange = 20000;
+        System.out.println(anyshortRange);
+        short randomShortRange = -12758;
+        System.out.println(randomShortRange);
+        int maxIntRange = 2147483647; //This piece of code is for the integer variables.
+        System.out.println(maxIntRange);
+        int minIntRange = -2147483648;
+        System.out.println(minIntRange);
+        int anyIntRange = 1234566;
+        System.out.println(anyIntRange);
+        int randomIntRange = -3456798;
+        System.out.println(randomIntRange);
+        long maxLongRange = 9223372036854775807L;// This piece of code is for the long variables.
+        System.out.println(maxLongRange);
+        long minLongRange = -9223372036854775807L;
+        System.out.println(minLongRange);
+        long anyLongRange = 4123456678922L;
+        System.out.println(anyLongRange);
+        long randomLongRange = -3456798999404L;
+        System.out.println(randomLongRange);
+        boolean myBooleanValue = true; // This piece of code is for the boolean variables.
+        System.out.println(myBooleanValue);
+        boolean iAmGoingToBe = true;
+        System.out.println(iAmGoingToBe);
+
+    }
 }
