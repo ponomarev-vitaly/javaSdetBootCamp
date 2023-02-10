79823a2224f3f417acbb008ecd1b1afab5c19300
ponomarev-vitaly <vp211082@gmail.com>
2023-02-10 04:04:29 +0300

    I created a new class SingleComment and completed an example N 6.

---
+++ src/week1/homework/SingleComment.java
@@ -0,0 +1,18 @@
+package week1.homework;
+/** 6-) Create an new class and main method with any name and copy below codes and correct the wrong comments
+
+        Java program to show multi line comments */
+public class SingleComment {
+
+
+    class SingleComment
+    {
+        public static void main(String args[])
+        {
+            System.out.println("Multi line comments below");
+            /** Comment line 1
+            Comment line 2
+            Comment line 3 */
+        }
+    }
+}
