83856a27b0fd501551ba387d6c06549bc01d472f
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 22:25:34 +0300

    I added several inputs for the width, length, height of the rectangular prism. I also added the formula for the prism's volume calculation.

--- src/week1/homework/PrismVolumeCalculator.java
+++ src/week1/homework/PrismVolumeCalculator.java
@@ -1,9 +1,21 @@
 package week1.homework;
-
+import java.util.Scanner;
 /*
 Type a program which calculates the volume of a rectangular
 prism whose length, with, and height are entered by user.
 Hint 1: Volume of a rectangular prism is width x length x height
  */
 public class PrismVolumeCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the width of the rectangular prism: ");
+        int prismWidth = scan.nextByte();
+        System.out.println("Please enter the length of the rectangular prism: ");
+        int prismLength = scan.nextByte();
+        System.out.println("Please enter the height of the rectangular prism: ");
+        int prismHeight = scan.nextByte();
+        int prismVolume = prismWidth * prismLength * prismHeight;
+        System.out.println("The volume of the rectangular prism is: " + prismVolume);
+
+    }
 }
