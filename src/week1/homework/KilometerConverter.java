2d8b3f3d298362a312d6a8812084f8a8b70f46d7
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 23:00:26 +0300

    I added class for converting miles in kilometers.

---
+++ src/week1/homework/KilometerConverter.java
@@ -0,0 +1,14 @@
+package week1.homework;
+import java.util.Scanner;
+/*
+7) Type a program which converts the mile to kilometer. Mile value will be
+entered by user. (Use double)
+Hint 1: km = mile x 1.6
+Hint 2: To get double, use nextDouble()
+ */
+public class KilometerConverter {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
