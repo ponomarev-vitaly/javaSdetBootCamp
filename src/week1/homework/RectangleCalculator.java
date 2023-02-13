e8e271a88208e8834ee2776cec7265e3ed0616af
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 21:14:38 +0300

    I added several formulas for the calculation of the area and the perimeter.

--- src/week1/homework/RectangleCalculator.java
+++ src/week1/homework/RectangleCalculator.java
@@ -7,5 +7,15 @@ Hint 1: Area of a rectangle is width x length
 Hint 2: Perimeter of a rectangle is 2x (width + length)
  */
 public class RectangleCalculator {
-
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the width of the rectangle: ");
+        int rectangleWidth = scan.nextInt();
+        System.out.println("Please enter the length of the rectangle: ");
+        int rectangleLength = scan.nextInt();
+        int rectangleArea = rectangleWidth * rectangleLength;
+        int rectanglePerimeter = 2 * (rectangleWidth + rectangleLength);
+        System.out.println("The area of the rectangle is: " + rectangleArea);
+        System.out.println("The perimeter of the rectangle is: " + rectanglePerimeter);
+    }
 }
