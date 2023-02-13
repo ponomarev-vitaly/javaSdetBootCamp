3d24b726a77f7e3ad7a3644fcbc290a8c121ae18
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 23:18:55 +0300

    I added a class to enter name and family name of the user.

---
+++ src/week1/homework/Names.java
@@ -0,0 +1,10 @@
+package week1.homework;
+
+/*
+9) Type a program which asks user to enter his/her first name and last
+name, then print it on the console.
+Hint: To get String, use nextLine()
+ */
+public class Names {
+
+}
