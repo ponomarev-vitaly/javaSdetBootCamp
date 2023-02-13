aaf52d3d3324a414a592a1ec0a1713cd4deb5401
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 23:10:42 +0300

    I added class for converting hours to seconds.

---
+++ src/week1/homework/TimeConverter.java
@@ -0,0 +1,15 @@
+package week1.homework;
+import java.util.Scanner;
+/*
+Type a program which converts the hours to seconds. Hours value will be
+entered by user. (Use long)
+Hint 1: second = hour x 60 x 60
+Hint 2: To get long, use
+nextLong()
+ */
+public class TimeConverter {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
