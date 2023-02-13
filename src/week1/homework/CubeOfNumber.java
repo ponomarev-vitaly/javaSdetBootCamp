3b5171f371e422c4d08e9b44381f69b8e9a148fd
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 20:35:19 +0300

    I added the formula for a calculation of a cube of a number.

--- src/week1/homework/CubeOfNumber.java
+++ src/week1/homework/CubeOfNumber.java
@@ -1,4 +1,17 @@
 package week1.homework;
-
+import java.util.Scanner;
+/*
+2) Type a program which calculates the cube of a number which is
+entered by user.
+Hint 1: Cube of a number is a x a x a
+ */
 public class CubeOfNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        int number = scan.nextInt();
+        int cubeOfNumber = number * number * number;
+        System.out.println("The program calculated the cube of a number successfully: " + cubeOfNumber);
+    }
+
 }
