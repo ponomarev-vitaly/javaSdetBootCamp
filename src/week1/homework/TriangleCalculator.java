982a7270410ee56e05199bc95735a1144bb2a0b7
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 22:55:45 +0300

    I added inputs for the sides of the triangle and the formula for the triangle's perimeter calculation.

--- src/week1/homework/TriangleCalculator.java
+++ src/week1/homework/TriangleCalculator.java
@@ -9,6 +9,14 @@ b + c Hint 4: To get byte, use nextByte()
 public class TriangleCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the length of the A side of the triangle: ");
+        byte triangleSideA = scan.nextByte();
+        System.out.println("Please enter the length of the B side of the triangle: ");
+        byte triangleSideB = scan.nextByte();
+        System.out.println("Please enter the length of the C side of the triangle: ");
+        byte triangleSideC = scan.nextByte();
+        byte trianglePerimeter = (byte) (triangleSideA + triangleSideB + triangleSideC);
+        System.out.println("The perimeter of the triangle is: " + trianglePerimeter);
 
     }
 }
