da8cc077751e7d140d72240a4df04c9a0c2b469f
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 23:24:41 +0300

    I added inputs for name and family name. I also added output for name and family name.

--- src/week1/homework/Names.java
+++ src/week1/homework/Names.java
@@ -1,10 +1,18 @@
 package week1.homework;
-
+import java.util.Scanner;
 /*
 9) Type a program which asks user to enter his/her first name and last
 name, then print it on the console.
 Hint: To get String, use nextLine()
  */
 public class Names {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String userName = scan.nextLine();
+        System.out.println("Please enter your family name: ");
+        String userFamilyName = scan.nextLine();
+        System.out.println("Here is the result: " + userName + " " + userFamilyName);
+    }
 
 }
