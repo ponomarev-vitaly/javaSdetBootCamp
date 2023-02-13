1086f24d23b0bcad25f1ce8b576f955cf8d0d9ea
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 22:11:53 +0300

    I added class for the calculation of the rectangular prism.

---
+++ src/week1/homework/PrismVolumeCalculator.java
@@ -0,0 +1,9 @@
+package week1.homework;
+
+/*
+Type a program which calculates the volume of a rectangular
+prism whose length, with, and height are entered by user.
+Hint 1: Volume of a rectangular prism is width x length x height
+ */
+public class PrismVolumeCalculator {
+}
