e97ccb61d9a21087692370cf81599019b04f0308
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 23:16:06 +0300

    I added an input for the hours and a formula for the calculation of the hours in seconds.

--- src/week1/homework/TimeConverter.java
+++ src/week1/homework/TimeConverter.java
@@ -10,6 +10,10 @@ nextLong()
 public class TimeConverter {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the number of hours: ");
+        long numberOfHours = scan.nextLong();
+        long numberOfSeconds = numberOfHours * 60 * 60;
+        System.out.println("The number of seconds equals to: " + numberOfSeconds);
 
     }
 }
