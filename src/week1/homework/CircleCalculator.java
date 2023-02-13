091b68e130804ad5043c4ac6aec9bef004cfa2ea
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 22:28:40 +0300

    I added class for the circle's area and perimeter calculation.

---
+++ src/week1/homework/CircleCalculator.java
@@ -0,0 +1,11 @@
+package week1.homework;
+/*
+5) Type a program which calculates the area and the perimeter of a circle
+whose radius is entered by user. (Use float)
+Hint 1: Take pi number as 3.14159
+Hint 2: Area of a circle is 3.14159 x radius x radius
+Hint 3: Perimeter of a circle is 2 x 3.14159 x radius
+Hint 4: To get float, use nextFloat()
+ */
+public class CircleCalculator {
+}
