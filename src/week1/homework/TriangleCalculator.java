d7786795ef97d52099b0185e71e3605b57fbd401
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 22:50:01 +0300

    I added class to calculate the perimeter of a triangle.

---
+++ src/week1/homework/TriangleCalculator.java
@@ -0,0 +1,14 @@
+package week1.homework;
+import java.util.Scanner;
+/*
+6) Type a program which calculates the perimeter of a triangle whose
+Side lengths are entered by user. (Use
+byte) Hint 1: Perimeter of a triangle is a +
+b + c Hint 4: To get byte, use nextByte()
+ */
+public class TriangleCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
