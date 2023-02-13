efc4651c595d268e7dfea4672285fc7635f503e1
ponomarev-vitaly <vp211082@gmail.com>
2023-02-13 20:53:39 +0300

    I added class for the rectangle calculator.

---
+++ src/week1/homework/RectangleCalculator.java
@@ -0,0 +1,11 @@
+package week1.homework;
+import java.util.Scanner;
+/*
+3) Type a program which calculates the area and the perimeter of a
+rectangle whose length and with are entered by user.
+Hint 1: Area of a rectangle is width x length
+Hint 2: Perimeter of a rectangle is 2x (width + length)
+ */
+public class RectangleCalculator {
+
+}
