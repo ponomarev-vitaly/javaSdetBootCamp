1ec83b7255a10e55a12985cf709da1d0ee1aa57c
ponomarev-vitaly <vp211082@gmail.com>
2023-02-14 06:20:01 +0300

    I added several If statements and comments.

--- src/week2/IfStatements.java
+++ src/week2/IfStatements.java
@@ -2,6 +2,18 @@ package week2;
 
 public class IfStatements {
     public static void main(String[] args) {
+        int num = 15;
+        if(num>10){
+            System.out.println("num is greater than 10");
+            System.out.println("num is greater than 10 that's why you are qualify to go to cinema");
+        }
+
+        if((num % 5) == 4){
+            //if the condition is true and then execute the following line's code
+            System.out.println("well done");
+        }
+        // if the condition is false, continue from the following line directly
+        System.out.println("the code continues from this line");
 
     }
 }
