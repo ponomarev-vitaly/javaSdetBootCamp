a7905feba2d80b773382bd5ca00f7ab7fbd3e37a
ponomarev-vitaly <vp211082@gmail.com>
2023-02-14 05:54:35 +0300

    I added decrement.

--- src/week2/IncrementAndDecrement.java
+++ src/week2/IncrementAndDecrement.java
@@ -10,5 +10,33 @@ public class IncrementAndDecrement {
         numA = 3;
         numA += 5; //--> numA = 8
         System.out.println(numA);
+
+        int numB = 2;
+        numB = numB * 4;
+        System.out.println(numB); // output --> 8
+        numB *= 4;
+        System.out.println(numB); // output --> 32
+
+        int numC = 7;
+        numC++;
+        System.out.println(numC);
+        int numD = 15;
+        numD++;
+        System.out.println(numD);
+
+    // Decrement
+        int numE = 6;
+        numE = numE - 3;
+        System.out.println(numE);
+
+        numE -= 2;
+        System.out.println(numE);
+
+        int numF = 15;
+        numF = numF / 5;
+        System.out.println(numF);
+        numF /= 3;
+        System.out.println(numF);
+
     }
 }
