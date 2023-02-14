740f21adbd18a4e3dbfc7bc8457d06770d4a89b5
ponomarev-vitaly <vp211082@gmail.com>
2023-02-14 05:45:13 +0300

    I added some changes.

--- src/week2/IncrementAndDecrement.java
+++ src/week2/IncrementAndDecrement.java
@@ -5,9 +5,10 @@ import java.sql.SQLOutput;
 public class IncrementAndDecrement {
     public static void main(String[] args) {
         int numA = 3;
-        numA = numA + 5;
+        numA = numA + 5; //--> numA = 8
         System.out.println(numA);
-        numA += 5;
+        numA = 3;
+        numA += 5; //--> numA = 8
         System.out.println(numA);
     }
 }
