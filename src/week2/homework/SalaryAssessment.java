b6cfd49dcb867a14a5989747c1ab5f2ac5597eff
ponomarev-vitaly <vp211082@gmail.com>
2023-02-15 02:52:33 +0300

    I added else if statements for a salary evaluation.

--- src/week2/homework/SalaryAssessment.java
+++ src/week2/homework/SalaryAssessment.java
@@ -1,4 +1,5 @@
 package week2.homework;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 /*
 Homework-1
@@ -8,13 +9,23 @@ write a program that annual earning
            Average start for work => 70 ~ 79
            below average start for work => 60 ~ 69
            Not acceptable =>  0 ~ 59
-*please just use if-else staement
-
- */
+*please just use if-else statement
+*/
 public class SalaryAssessment {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
+        System.out.println("Please enter your salary level (from 0 to 100): ");
+        int salary = scan.nextInt();
+        if(salary >= 100){
+            System.out.println("Perfect start for work");
+        }else if(salary >= 80){
+            System.out.println("Good start for work");
+        }else if(salary >= 70){
+            System.out.println("Average start for work");
+        }else if(salary >= 60){
+            System.out.println("Below average start for work");
+        }else if(salary >= 0){
+            System.out.println("Not acceptable");
+        }
     }
 }
