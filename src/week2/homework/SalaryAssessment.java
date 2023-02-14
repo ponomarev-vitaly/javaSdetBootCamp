a139fde5eb247d294478db97b6cfc7170db7052c
ponomarev-vitaly <vp211082@gmail.com>
2023-02-15 02:40:07 +0300

    I added the class to assess salary. The user is supposed to enter the meaning of the salary. The program will evaluate the earnings.

---
+++ src/week2/homework/SalaryAssessment.java
@@ -0,0 +1,20 @@
+package week2.homework;
+import java.util.Scanner;
+/*
+Homework-1
+write a program that annual earning
+           Perfect start for work => =>100
+           Good start for work => 80 ~ 99
+           Average start for work => 70 ~ 79
+           below average start for work => 60 ~ 69
+           Not acceptable =>  0 ~ 59
+*please just use if-else staement
+
+ */
+public class SalaryAssessment {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
