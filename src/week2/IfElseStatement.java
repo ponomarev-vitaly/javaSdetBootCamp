a64346c623c14ebd882b4ed30412bb06178a88fd
ponomarev-vitaly <vp211082@gmail.com>
2023-02-14 06:26:37 +0300

    I added If/Else statements.

---
+++ src/week2/IfElseStatement.java
@@ -0,0 +1,15 @@
+package week2;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        int number = 0;
+        if(number > 0){
+            System.out.println("The number is positive.");
+        }
+        else{
+            System.out.println("The number is not positive.");
+        }
+
+        System.out.println("This code line is always executed.");
+    }
+}
