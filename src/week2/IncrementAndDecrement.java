7a0429eb4c05e02cf8098edebfd8e96d6f1a3326
ponomarev-vitaly <vp211082@gmail.com>
2023-02-14 05:43:00 +0300

    I added class with the increment and decrement.

---
+++ src/week2/IncrementAndDecrement.java
@@ -0,0 +1,13 @@
+package week2;
+
+import java.sql.SQLOutput;
+
+public class IncrementAndDecrement {
+    public static void main(String[] args) {
+        int numA = 3;
+        numA = numA + 5;
+        System.out.println(numA);
+        numA += 5;
+        System.out.println(numA);
+    }
+}
