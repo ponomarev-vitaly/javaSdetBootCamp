fb061d4b2b61f81dcd1346972e21b0a60eac5bdf
ponomarev-vitaly <vp211082@gmail.com>
2023-02-14 07:00:07 +0300

    I added if and else statements.

--- src/week2/IfElseIfStatements.java
+++ src/week2/IfElseIfStatements.java
@@ -2,6 +2,18 @@ package week2;
 
 public class IfElseIfStatements {
     public static void main(String[] args) {
+        int percentage = 77;
+        char grade;
+        if(percentage >= 90){
+            grade = 'A';
+        }else if(percentage >= 75){
+            grade = 'B';
+        }else if(percentage >= 65){
+            grade = 'C';
+        }else grade = 'F';
+
+        System.out.println(grade);
+        System.out.println("This codeline we executed for any condition...");
 
     }
 
