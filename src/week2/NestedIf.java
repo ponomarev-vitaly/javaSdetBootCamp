4711ec5190693e9d3f637e3da28da4d89de0f8e3
ponomarev-vitaly <vp211082@gmail.com>
2023-02-15 04:59:30 +0300

    I added nested if statements.

--- src/week2/NestedIf.java
+++ src/week2/NestedIf.java
@@ -3,5 +3,21 @@ package week2;
 public class NestedIf {
     public static void main(String[] args) {
 
+        boolean male = false;
+        int age = 30;
+        if (male){
+            if (age < 20){
+                System.out.println("Boy");
+            }else{
+                System.out.println("Man");
+            }
+        }else{
+            if (age < 20){
+                System.out.println("Girl");
+            }else{
+                System.out.println("Woman");
+            }
+        }
+
     }
 }
