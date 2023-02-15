d54af52c902e62d059c953ee67e9b940bd460d42
ponomarev-vitaly <vp211082@gmail.com>
2023-02-16 01:36:22 +0300

    I added new class for students' evaluation.

---
+++ src/week2/homework/StudentsEvaluation.java
@@ -0,0 +1,23 @@
+package week2.homework;
+import java.util.Scanner;
+
+/*
+Homework-4
+declare variable year and department
+years should be 1,2,3,4
+deparments Computer and Machine Learning
+Use the first if statement to check the department of the student.
+Use the inner if statement to check in which year the student is.
+Display the result.
+i.e;
+The student is in the Computer Science department
+The student is in 2nd year
+Please use nested if
+ */
+
+public class StudentsEvaluation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
