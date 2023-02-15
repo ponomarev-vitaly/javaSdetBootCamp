f1e16f7f9387d91ff69c3ddd1051cee1f21eba36
ponomarev-vitaly <vp211082@gmail.com>
2023-02-16 01:30:43 +0300

    I added nested if statement to evaluate the candidate for blood donation.

--- src/week2/homework/BloodDonation.java
+++ src/week2/homework/BloodDonation.java
@@ -1,4 +1,5 @@
 package week2.homework;
+import java.util.Scanner;
 /*
 Homework-3
 Assign age and weight,
@@ -10,4 +11,19 @@ for blood donation,
  */
 
 public class BloodDonation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+        int age = scan.nextInt();
+        System.out.println("Please enter your weight in pounds: ");
+        int weight = scan.nextInt();
+        if(age>21){
+            if(weight>90)
+                System.out.println("Congratulations! You are a good candidate for blood donation! Please help people!");
+            else
+                System.out.println("Your weight must be more than 90 pounds! Come again in one month.");
+        }
+        else
+            System.out.println("You are too young yet! Come again in a year!");
+    }
 }
