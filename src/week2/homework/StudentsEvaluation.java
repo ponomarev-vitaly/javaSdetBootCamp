9995b6511072b6f338da3f617f4fc2638ae2da27
ponomarev-vitaly <vp211082@gmail.com>
2023-02-16 02:20:31 +0300

    I added if, else if statements to check the year of the Student and the Department.

--- src/week2/homework/StudentsEvaluation.java
+++ src/week2/homework/StudentsEvaluation.java
@@ -18,6 +18,32 @@ Please use nested if
 public class StudentsEvaluation {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the year of your course (years should be 1,2,3,4): ");
+        int year = scan.nextInt();
+        System.out.println("Please enter your department: 1 - Computer and Machine Learning Department, 2 - Computer Science Department");
+        int department = scan.nextInt();
+        if (department == 1){
+            if (year==1){
+                System.out.println("You are the " + year + "st year Student. Your Department is Computer and Machine Learning Department.");
+            } else if (year == 2){
+                System.out.println("You are the " + year + "nd year Student. Your Department is Computer and Machine Learning Department.");
+            } else if (year == 3){
+                System.out.println("You are the " + year + "rd year Student. Your Department is Computer and Machine Learning Department.");
+            } else if (year == 4){
+                System.out.println("You are the " + year + "th year Student. Your Department is Computer and Machine Learning Department.");
+            }
+        } else if (department == 2) {
+            if (year==1){
+                System.out.println("You are the " + year + "st year Student. Your Department is Computer Science Department.");
+            } else if (year == 2){
+                System.out.println("You are the " + year + "nd year Student. Your Department is Computer Science Department.");
+            } else if (year == 3){
+                System.out.println("You are the " + year + "rd year Student. Your Department is Computer Science Department.");
+            } else if (year == 4){
+                System.out.println("You are the " + year + "th year Student. Your Department is Computer Science Department.");
+            }
+        }  else
+           System.out.println("You are not the Student of the Computer and Machine Learning Department OR Computer Science Department OR the year of your course is not accepted yet.");
 
     }
 }
