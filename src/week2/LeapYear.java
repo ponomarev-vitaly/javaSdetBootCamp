891c59c99012d5b14fc04af6f320808a15f67f19
ponomarev-vitaly <vp211082@gmail.com>
2023-02-15 06:20:06 +0300

    I added program to check whether it's a leap year or not.

---
+++ src/week2/LeapYear.java
@@ -0,0 +1,20 @@
+package week2;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        /*Question 1:
+        Type java code by using nested ternary.
+        Write a program to check if a year is leap year or not.
+        If the year is divisible by 100 then it must be divisible by 400.
+        If a year is not divisible by 100 then it must be divisible by 4.
+         */
+
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+    }
+}
