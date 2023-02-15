1446692d66471bb9fa8bfa0bb2b8336f159ae8ca
ponomarev-vitaly <vp211082@gmail.com>
2023-02-16 01:21:08 +0300

    I added new class for blood donation assessment.

---
+++ src/week2/homework/BloodDonation.java
@@ -0,0 +1,13 @@
+package week2.homework;
+/*
+Homework-3
+Assign age and weight,
+for blood donation,
+ people should be bigger than 21
+ and
+ more than 90 pound
+ please use nested if
+ */
+
+public class BloodDonation {
+}
