01bcc7c79146b2012b45656369ae527e4db2b5f5
ponomarev-vitaly <vp211082@gmail.com>
2023-02-16 02:36:19 +0300

    I added a program which displays numbers from 1 to 100.

---
+++ src/week2/homework/DisplayNumbers.java
@@ -0,0 +1,14 @@
+package week2.homework;
+/*
+1-) Write a java program to print integers from 1 to 100
+ */
+public class DisplayNumbers {
+    public static void main(String[] args) {
+        int number = 100;
+        System.out.println("List of numbers from 1 to 100: ");
+        for (int i = 1; i <= number; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
--- src/week2/homework/PrintingIntegers.java
+++
@@ -1,6 +0,0 @@
-package week2.homework;
-/*
-1-) Write a java program to print integers from 1 to 100
- */
-public class PrintingIntegers {
-}
