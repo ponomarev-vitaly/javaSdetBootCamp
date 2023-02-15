a79c9b1298758cd12a8454f3b8d4bd8357b0cea0
ponomarev-vitaly <vp211082@gmail.com>
2023-02-15 05:57:10 +0300

    I added Example 1 and Example 2 for ternary operator.

--- src/week2/TurneryOperator.java
+++ src/week2/TurneryOperator.java
@@ -2,6 +2,14 @@ package week2;
 
 public class TurneryOperator {
     public static void main(String[] args) {
+        // Example 1.
+        int y = 10;
+        int x = ( y > 5 ) ? (y*2):( 3 * y );
+        System.out.println("x is equal to " + x);
+        // Example 2.
+        y = 7;
+        x = ( y < 5 ) ? ( y * 2 ):( 3 * y );
+        System.out.println("x is equal to " + x + " in example two");
 
     }
 }
