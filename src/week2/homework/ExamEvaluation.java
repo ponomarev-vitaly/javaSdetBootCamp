f481d64f7cbb3eea35c6579a1a0ca6c08e37e597
ponomarev-vitaly <vp211082@gmail.com>
2023-02-15 03:58:13 +0300

    I added an instruction for the user.

--- src/week2/homework/ExamEvaluation.java
+++ src/week2/homework/ExamEvaluation.java
@@ -5,7 +5,7 @@ Homework-2
 assign int mid term exam result between 0-100, int mtExam
 then assign final exam result between 0-100, int
 then calculate mid term exam of %40,and final exam %60,
-if it is more than 70, print result as "Passed".
+if it is more than 70, print result as "Passed" and if it is less than 71, print the result.
  */
 public class ExamEvaluation {
     public static void main(String[] args) {
@@ -15,10 +15,10 @@ public class ExamEvaluation {
         System.out.println("Please enter the final exam result from 0 to 100: ");
         int finalExam = scan.nextInt();
         int termScore = (int) ((midTermExam * 0.4) + (finalExam * 0.6));
-        if(termScore >= 70){
+        if(termScore > 70){
             System.out.println("You passed!");
         }else{
-            System.out.println("You did not pass this time.");
+            System.out.println("You did not pass this time. Give it another try!");
         }
     }
 }
