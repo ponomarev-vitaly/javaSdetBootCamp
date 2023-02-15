c04ccc95156035ef1159a1a436c0eb7e502c11be
ponomarev-vitaly <vp211082@gmail.com>
2023-02-15 06:51:57 +0300

    I added class for nested ternary operator.

---
+++ src/week2/NestedTernaryOperator.java
@@ -0,0 +1,18 @@
+package week2;
+
+import java.util.Scanner;
+
+public class NestedTernaryOperator {
+    public static void main(String[] args) {
+        /*
+        How to find the largest of 3 numbers using the nested ternary operator.
+         */
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Please enter 3 numbers: ");
+        int num1, num2, num3;
+        num1 = scan.nextInt();
+        num2 = scan.nextInt();
+        num3 = scan.nextInt();
+
+    }
+}
