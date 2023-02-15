1fa28e8a184eb483694ab417faaa02214ec30a5c
ponomarev-vitaly <vp211082@gmail.com>
2023-02-15 03:00:47 +0300

    I added the class for the exams' evaluation.

---
+++ src/week2/homework/ExamEvaluation.java
@@ -0,0 +1,22 @@
+package week2.homework;
+import java.util.Scanner;
+/*
+Homework-2
+assign int mid term exam result between 0-100, int mtExam
+then assign final exam result between 0-100, int
+then calculate mid term exam of %40,and final exam %60,
+if it is more than 70, print result as "Passed".
+ */
+public class ExamEvaluation {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the mid term exam result between 0-100: ");
+        int midTermExam = scan.nextInt();
+        System.out.println("Please enter the final exam result: ");
+        int finalExam = scan.nextInt();
+
+
+
+    }
+}
