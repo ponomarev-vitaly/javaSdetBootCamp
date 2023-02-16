d0ce867344e2ee67562d8fed9a0bc9133795fbb5
ponomarev-vitaly <vp211082@gmail.com>
2023-02-16 04:06:36 +0300

    I added class for converting Fahrenheit temperature to Celcius temperature.

---
+++ src/week2/homework/TemperatureConverter.java
@@ -0,0 +1,9 @@
+package week2.homework;
+/*
+3- Write a java program to convert Fahrenheit to Celsius using user input.
+    Formula: Celsius= (Fahrenheit-32)*5/9
+    Given : 87F
+    Calculation:  (87-32)*5/9=30.5 Celcius
+ */
+public class TemperatureConverter {
+}
