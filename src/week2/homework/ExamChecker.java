8df547c5601530b96a180319f448a6e11d77fc88
ponomarev-vitaly <vp211082@gmail.com>
2023-02-17 02:24:35 +0300

    I added class to check the student's mark.

---
+++ src/week2/homework/ExamChecker.java
@@ -0,0 +1,16 @@
+package week2.homework;
+import java.util.Scanner;
+/*
+7- Write a java program to find if the student is passed the exam (user scanner class for user inputs)
+    Note: passing mark is : 65
+ */
+
+public class ExamChecker {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("Please enter your mark: ");
+        int mark = scan.nextInt();
+
+    }
+}
+
