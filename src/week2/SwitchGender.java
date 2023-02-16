87440337de3703ce5c56fefaca128c5f7d7c4bd0
ponomarev-vitaly <vp211082@gmail.com>
2023-02-16 06:55:08 +0300

    I added a possibility to check a gender with switch and gender.

--- src/week2/SwitchGender.java
+++ src/week2/SwitchGender.java
@@ -4,15 +4,18 @@ public class SwitchGender {
     public static void main(String[] args) {
         String gender = "male and female";
 
-        switch(gender){
-            case "female":
-                System.out.println("This is a girl.");
-                break;
-            case "male":
-                System.out.println("This is a boy.");
-                break;
-            default:
-                System.out.println("Please enter valid gender.");
+        switch (gender) {
+            case "female" -> System.out.println("This is a girl.");
+            case "male" -> System.out.println("This is a boy.");
+            default -> System.out.println("Please enter valid gender.");
+        }
+
+        if (gender.equals("female")) {
+            System.out.println("This is a girl.");
+        } else if (gender.equals("male")) {
+            System.out.println("This is a boy.");
+        } else {
+            System.out.println("Please enter valid gender.");
         }
     }
 }
