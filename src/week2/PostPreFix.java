e7645ccdda3406d16ac9cc5a60cd2704da346e5e
ponomarev-vitaly <vp211082@gmail.com>
2023-02-16 05:48:08 +0300

    I added interesting examples with postfix and prefix.

--- src/week2/PostPreFix.java
+++ src/week2/PostPreFix.java
@@ -10,6 +10,9 @@ public class PostPreFix {
         // If there is a string or text and then int or any number continue in the same code line (as the same line 7 and 8)
         // Java is converting int to String automatically.
 
+        a = 7;
+        b = 3;
+
         //** if there is int and then string is coming in the same line (as the same line 15 and 16)
         // Java is keeping int variable as an int.
         System.out.println(a++ + a + "Postfix a++"); // 15Postfix a++ :
