4605473f689bae740df04806a44fe7d5bf12b5f7
ponomarev-vitaly <vp211082@gmail.com>
2023-02-16 05:29:11 +0300

    I added new class with postfix and prefix.

---
+++ src/week2/PostPreFix.java
@@ -0,0 +1,11 @@
+package week2;
+
+public class PostPreFix {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        System.out.println("Postfix a++" + a++); //
+        System.out.println("Prefix ++b" + ++b); //
+    }
+
+}
