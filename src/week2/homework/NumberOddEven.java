176e71e20467c47eb9fb7dab971fbfe2d4a03d26
ponomarev-vitaly <vp211082@gmail.com>
2023-02-17 02:56:57 +0300

    I added if statement to check the number whether it's even or odd. Important: the statement devision on 0 didn't work.

--- src/week2/homework/NumberOddEven.java
+++ src/week2/homework/NumberOddEven.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 8- Write a java program to find the given number is odd or even (user scanner class for user inputs)
  */
 public class NumberOddEven {
-    Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.printf("Please enter the number: ");
+        Scanner scan = new Scanner(System.in);
+        byte number = scan.nextByte();
+        int remainder = number % 2;
+        if (remainder == 0) {
+            System.out.printf("You entered even number: " + number);
+        } else if (remainder > 0) {
+            System.out.printf("You entered odd number: " + number);
+        }  // else if (number == 0)
+        // System.out.printf("Probably you have entered 0. Give it another try!");
 
+    }
 }
