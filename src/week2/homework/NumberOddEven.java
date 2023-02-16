694fbd73cfc77d4e4f4873f5568390e08bb1c492
ponomarev-vitaly <vp211082@gmail.com>
2023-02-17 02:32:41 +0300

    I added class to check a number whether it's odd or even.

---
+++ src/week2/homework/NumberOddEven.java
@@ -0,0 +1,9 @@
+package week2.homework;
+import java.util.Scanner;
+/*
+8- Write a java program to find the given number is odd or even (user scanner class for user inputs)
+ */
+public class NumberOddEven {
+    Scanner scan = new Scanner(System.in);
+
+}
