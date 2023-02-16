3d552e98ed68614eb40fe06fcf37d740a2e11b59
ponomarev-vitaly <vp211082@gmail.com>
2023-02-16 04:46:58 +0300

    I added false and true conditions.

--- src/week2/LogicalOperators.java
+++ src/week2/LogicalOperators.java
@@ -1,4 +1,36 @@
 package week2;
 
 public class LogicalOperators {
+    /* Logical Operators
+    1) && -> "And" operator
+        true && true --> true
+        true && false --> false
+        false && true --> false
+        false && false --> false
+        Note: If 0 is false and 1 is true, "&&" operation is similar to multiplication of 0 and 1.
+
+     */
+    public static void main(String[] args) {
+        if(5>7 && 10>6) // False && True --> False
+            System.out.println("Apple: ");
+        else
+            System.out.println("Grape: ");
+
+        if(5>7 && 10<6) // False && False --> False
+            System.out.println("Apple: ");
+        else
+            System.out.println("Grape: ");
+
+        if (5 < 7 && 10 < 6) // True && True --> True
+            System.out.println("Apple: ");
+        else
+            System.out.println("Grape: ");
+
+        if (5<7 && 10<6) // True && False --> False
+            System.out.println("Apple: ");
+        else
+            System.out.println("Grape: ");
+
+    }
+
 }
