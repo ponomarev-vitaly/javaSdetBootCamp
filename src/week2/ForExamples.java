dad22f01f63fea14a88e99728e50424d48b90c17
ponomarev-vitaly <vp211082@gmail.com>
2023-02-17 06:39:31 +0300

    I added for loop to calculate factorials.

--- src/week2/ForExamples.java
+++ src/week2/ForExamples.java
@@ -1,10 +1,16 @@
 package week2;
+import java.util.Scanner;
+
 /*
 5! = 5*4*3*2*1=120
 
  */
 public class ForExamples {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the number from 1 to 10: ");
+        int fact = scan.nextInt();
+        for(int mult = 1; )
 
     }
 }
