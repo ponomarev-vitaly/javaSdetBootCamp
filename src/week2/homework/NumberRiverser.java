e3998d94bbaf08b4446bdcd2ef6c1708730f757d
ponomarev-vitaly <vp211082@gmail.com>
2023-02-17 03:00:22 +0300

    I added new class to reverse number.

---
+++ src/week2/homework/NumberRiverser.java
@@ -0,0 +1,14 @@
+package week2.homework;
+import java.util.Scanner;
+/*
+9- Write a java program to reverse a given number (user scanner class for user inputs)
+input: 45678 , Output: 87654
+ */
+public class NumberRiverser {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        int number = scan.nextInt();
+
+    }
+}
