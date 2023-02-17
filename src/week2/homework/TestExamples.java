51b4f5b48b2556d39c647b0238d4d636fc91671e
ponomarev-vitaly <vp211082@gmail.com>
2023-02-18 01:48:34 +0300

    I added new class for checking test examples.

--- src/week2/homework/TestExamples.java
+++ src/week2/homework/TestExamples.java
@@ -1,6 +1,42 @@
 package week2.homework;
 
 public class TestExamples {
+    public static void main(String[] args) {
+
+//        int x = 0;
+//        if (x < 4) {
+//            x = x + 1;
+//        }
+//        System.out.println("x is " + x);
+
+//        if (false)System.out.print("Hello");
+//        else System.out.print("Else block is executed");
+
+//        int income = 4001;
+//        if (income > 3000) {
+//            System.out.println("Income is greater than 3000");
+//        }
+//        else if (income > 4000) {
+//            System.out.println("Income is greater than 4000");
+//        }
+
+//        double temperature = 50;
+//
+//        if (temperature >= 100)
+//            System.out.println("too hot");
+//
+//        else if (temperature <= 40)
+//
+//            System.out.println("too cold");
+//
+//        else
+//
+//            System.out.println("just right");
+
+        int x = 0;
+        int y = (x > 0) ? 10 : -10;
+        System.out.println(y);
 
 
+    }
 }
