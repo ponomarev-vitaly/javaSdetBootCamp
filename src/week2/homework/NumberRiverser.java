7f140150dcf95cd54a02980a21e936a4ddf71c90
ponomarev-vitaly <vp211082@gmail.com>
2023-02-17 04:12:52 +0300

    I added class the riversing numbers. First I added if statement to check how many digits are there in the number user entered.

--- src/week2/homework/NumberRiverser.java
+++ src/week2/homework/NumberRiverser.java
@@ -9,6 +9,11 @@ public class NumberRiverser {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter the number: ");
         int number = scan.nextInt();
+        if (number >= -9 && number <= 9 ) {
+            System.out.println("Single-digit number! Can not riverse rhe number!" + number);
+        } // else {
+        //            for() {
+        //            }
 
     }
 }
