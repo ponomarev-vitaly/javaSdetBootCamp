5ef30b6cf5011e57d9408222eec21b1d66308ee1
ponomarev-vitaly <vp211082@gmail.com>
2023-02-17 07:06:47 +0300

    I added new class for the multiplication table.

---
+++ src/week2/homework/MultiplicationTable.java
@@ -0,0 +1,15 @@
+package week2.homework;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // print the multiplication table
+        /*
+        1*1=1
+        1*2=2
+        1*3=3
+        1*4=4
+        1*10=10
+        */
+
+    }
+}
