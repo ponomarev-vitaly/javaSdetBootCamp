499ed1621a92c38bac252ab8dad45980e37342e4
ponomarev-vitaly <vp211082@gmail.com>
2023-02-18 06:49:16 +0300

    I added new class for the while loop.

---
+++ src/week2/WhileLoop.java
@@ -0,0 +1,12 @@
+package week2;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<=5){
+
+            System.out.println(i);
+            i++;
+        }
+    }
+}
