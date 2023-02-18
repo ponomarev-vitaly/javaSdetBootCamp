f476e55914c4e9bf651f13e0f71008cfdbd52ccd
ponomarev-vitaly <vp211082@gmail.com>
2023-02-18 06:18:15 +0300

    I added class for the new inverted pyramid.

---
+++ src/week2/homework/InvertedPyramidFromNumbers.java
@@ -0,0 +1,20 @@
+package week2.homework;
+/*
+Inverted half pyramid using numbers
+result should be like this;
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+ */
+public class InvertedPyramidFromNumbers {
+    public static void main(String[] args) {
+        for(int i = 5; i > 0; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println(); //The cursor is jumping to the next line.
+        }
+    }
+}
