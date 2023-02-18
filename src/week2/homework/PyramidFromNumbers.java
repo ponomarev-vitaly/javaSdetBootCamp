b7a40798a8f8cac7eed8e6aabf676b7b815068cd
ponomarev-vitaly <vp211082@gmail.com>
2023-02-18 06:13:13 +0300

    I added class for building up pyramid from numbers.

---
+++ src/week2/homework/PyramidFromNumbers.java
@@ -0,0 +1,20 @@
+package week2.homework;
+/*
+Program to print half pyramid a using numbers
+result should be like this;
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+ */
+public class PyramidFromNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(); //The cursor is jumping to the next line.
+        }
+    }
+}
