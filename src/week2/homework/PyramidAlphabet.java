28fdaaf57eb56187d20b70028394c6c18404d12c
ponomarev-vitaly <vp211082@gmail.com>
2023-02-18 06:41:01 +0300

    I added for loops to display capital letters on the screen.

--- src/week2/homework/PyramidAlphabet.java
+++ src/week2/homework/PyramidAlphabet.java
@@ -10,12 +10,14 @@ E E E E E
  */
 public class PyramidAlphabet {
     public static void main(String[] args) {
-        for (int i = 1; i < 6; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println(); //The cursor is jumping to the next line.
-        }
 
+        System.out.println((char)64); // A,65 - B,66 - C,67
+        for(int i=1; i<=5; i++){
+
+            for(int j=1; j<=i; j++){
+                System.out.print((char)(64 + i));
+            }
+            System.out.println();
+        }
     }
 }
