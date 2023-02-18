49297f7ad75e92b0b0ac5c9ab02523108c48c9dc
ponomarev-vitaly <vp211082@gmail.com>
2023-02-18 06:04:29 +0300

    I refreshed code just a little bit.

--- src/week2/homework/Pyramid.java
+++ src/week2/homework/Pyramid.java
@@ -10,8 +10,8 @@ result should be like this;
  */
 public class Pyramid {
     public static void main(String[] args) {
-        for(int i = 1; i < 6; i++){
-            for(int j = 1; j <= i; j++){
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println(); //The cursor is jumping to the next line.
