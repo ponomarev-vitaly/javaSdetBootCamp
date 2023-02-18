ca47d50ec457be7519a2827e953c3593ffea73aa
ponomarev-vitaly <vp211082@gmail.com>
2023-02-18 05:52:20 +0300

    I added space after star sign.

--- src/week2/homework/Pyramid.java
+++ src/week2/homework/Pyramid.java
@@ -12,7 +12,7 @@ public class Pyramid {
     public static void main(String[] args) {
         for(int i = 1; i < 6; i++){
             for(int j = 1; j <= i; j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println(); //The cursor is jumping to the next line.
         }
