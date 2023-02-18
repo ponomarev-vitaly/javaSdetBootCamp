51300441977365ba0d69be826ceb8dc7f0adcb3d
ponomarev-vitaly <vp211082@gmail.com>
2023-02-18 06:53:33 +0300

    I added some new changes to while loop.

--- src/week2/WhileLoop.java
+++ src/week2/WhileLoop.java
@@ -4,9 +4,10 @@ public class WhileLoop {
     public static void main(String[] args) {
         int i = 1;
         while (i<=5){
-
-            System.out.println(i);
+            if(i!=3) System.out.println(i);
             i++;
+            if(i==5)
+            break;
         }
     }
 }
