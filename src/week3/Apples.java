8c4e161cb6161b2e348e6d9b86f1e58dba72c13c
ponomarev-vitaly <vp211082@gmail.com>
2023-02-22 00:34:22 +0300

    I added new class for just an example of loop.

---
+++ src/week3/Apples.java
@@ -0,0 +1,9 @@
+package week3;
+
+public class Apples {
+    public static void main(String[] args) {
+        for(int counter=1; counter<=10; counter++){
+            System.out.println(counter);
+        }
+    }
+}
