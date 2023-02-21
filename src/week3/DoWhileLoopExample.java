ef70706f15f9010ace48668bc40b3a5ad9384456
ponomarev-vitaly <vp211082@gmail.com>
2023-02-22 00:59:13 +0300

    I added a simple example of do while loop.

---
+++ src/week3/DoWhileLoopExample.java
@@ -0,0 +1,12 @@
+package week3;
+
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        int counter = 15;
+
+        do{
+            System.out.println(counter);
+            counter++;
+        }while(counter<=10);
+    }
+}
