14600756d2afd054b572083156aa2a45bef2be04
ponomarev-vitaly <vp211082@gmail.com>
2023-02-22 00:52:18 +0300

    I added a simple example of while loop.

---
+++ src/week3/WhileLoopExample.java
@@ -0,0 +1,11 @@
+package week3;
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter<10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
