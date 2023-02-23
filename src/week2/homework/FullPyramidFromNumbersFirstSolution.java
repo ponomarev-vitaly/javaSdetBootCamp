4bc874be95ef09bc46915d55e8d23dfabba099ce
ponomarev-vitaly <vp211082@gmail.com>
2023-02-24 01:03:05 +0300

    I added new class to build full pyramid from numbers. I added some code for the stable meaning of rows = 5.

---
+++ src/week2/homework/FullPyramidFromNumbersFirstSolution.java
@@ -0,0 +1,38 @@
+package week2.homework;
+/*
+Program to print pyramid using numbers
+result should be like this;
+        1
+      2 3 2
+    3 4 5 4 3
+  4 5 6 7 6 5 4
+5 6 7 8 9 8 7 6 5
+ */
+public class FullPyramidFromNumbersFirstSolution {
+    public static void main(String[] args) {
+        int rows = 5, k = 0, count = 0, count1 = 0;
+
+        for (int i = 1; i <= rows; ++i) {
+            for (int space = 1; space <= rows - i; ++space) {
+                System.out.print("  ");
+                ++count;
+            }
+
+            while (k != 2 * i - 1) {
+                if (count <= rows - 1) {
+                    System.out.print((i + k) + " ");
+                    ++count;
+                } else {
+                    ++count1;
+                    System.out.print((i + k - 2 * count1) + " ");
+                }
+
+                ++k;
+            }
+            count1 = count = k = 0;
+
+            System.out.println();
+        }
+    }
+
+}
