42755ec7a50cba8456dd8fb603356dc17fab8cf0
ponomarev-vitaly <vp211082@gmail.com>
2023-02-23 03:33:04 +0300

    I added code to calculate a factorial and display the result.

--- src/week3/WhileLoop.java
+++ src/week3/WhileLoop.java
@@ -1,5 +1,26 @@
 package week3;
 
-public class WhileLoop {
+import java.util.Scanner;
 
+public class WhileLoop {
+    public static void main(String[] args) {
+        // Find the factorial for the given number
+        /*
+        5! = 5*4*3*2*1=120
+        8! = 8*7*6*5*4*3*2*1
+         */
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("Please enter the factorial number to calculate: ");
+
+        int fact = scan.nextInt(); // 7! = ? --> 7*6*5*4*3*2*1
+        int result =1;
+        int counter = 0;
+        while(fact >= 1){
+           result = result * fact;
+           fact--;
+           counter++;
+           System.out.println(result);
+        }
+        System.out.println(counter + "! = " + result);
+    }
 }
