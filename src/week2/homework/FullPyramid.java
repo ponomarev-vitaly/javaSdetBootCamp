9acd599c0af75d502d113a538c6858ae8c9792f1
ponomarev-vitaly <vp211082@gmail.com>
2023-02-23 20:28:08 +0300

    I added an input from the keyboard to let user display any symbol.

--- src/week2/homework/FullPyramid.java
+++ src/week2/homework/FullPyramid.java
@@ -11,9 +11,17 @@ result should be like this;
  */
 public class FullPyramid {
     public static void main(String[] args) {
+        // Give information for the user to enter the number of rows.
         Scanner scan = new Scanner(System.in);
-        System.out.printf("Please enter the number of rows you want to display: ");// Give information for the user to enter the number of rows.
+        System.out.printf("Please enter the number of rows you want to display: ");
         int rows = scan.nextInt();
+
+        /* Give directions to the user to enter the symbol from the keyboard.
+
+         */
+        Scanner scan1=new Scanner(System.in);
+        System.out.printf("Please enter the symbol you want to print: ");
+        String symbol = scan1.nextLine();
 
         for (int i = 0; i < rows; i++){
             for (int k = ((rows - i)*2-1); k > 1; k--) { // This loop displays " "
@@ -21,7 +29,7 @@ public class FullPyramid {
             }
 
             for (int j = 0; j < (2*i+1); j++){ // This loop displays "*"
-                System.out.print("* ");
+                System.out.print(symbol + " ");
             }
 
             System.out.println();
