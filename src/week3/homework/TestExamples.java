65846c162f9a9c3270dada892b5a315f948cd8dc
ponomarev-vitaly <vp211082@gmail.com>
2023-02-25 02:59:42 +0300

    I added new class to check the code from the Java tests.

--- src/week3/homework/TestExamples.java
+++ src/week3/homework/TestExamples.java
@@ -1,4 +1,11 @@
 package week3.homework;
 
 public class TestExamples {
+    public static void main(String[] args) {
+        int count = 0;
+        do {
+            System.out.println("Welcome to Java");
+            count++;
+        } while (count < 10);
+    }
 }
