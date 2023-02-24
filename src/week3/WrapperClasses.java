90c6a83b9bd7c2e1e5ffd73d9216c821bb676432
ponomarev-vitaly <vp211082@gmail.com>
2023-02-24 06:27:01 +0300

    I added some more examples with a converting between different data types.

--- src/week3/WrapperClasses.java
+++ src/week3/WrapperClasses.java
@@ -7,6 +7,7 @@ public class WrapperClasses {
         Character myChar= '@';
         String intToString;
         int num = 19;
+        double dNum = 16.3;
 
         System.out.println(myInt);
         System.out.println(myDouble);
@@ -14,5 +15,34 @@ public class WrapperClasses {
         myInt.toString();
         intToString = myInt.toString();
         intToString.length(); // number of intToString characters --> 1
+
+        System.out.println("Primitive value " + num);
+        System.out.println("Wrapper value " + myInt);
+        System.out.println(Double.max(4.6, 11.7)); // --> 11.7
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+
+        // Unboxing
+        myDouble = dNum;
+        System.out.println(myDouble);
+
+        // Autoboxing
+        dNum = myDouble;
+        System.out.println(dNum);
+
+        // Parsing converts String values to the primitive types.
+        String number = "1234";
+        System.out.println(number + 6); // Converting integer 6 to string, after that concatenation proceeds, we get the result 12346.
+        System.out.println(6 + number); // The same result as the previous, but with the different order: 61234.
+        // int numerV = number; // This is not balancing or matching, in that case we get Compile time error.
+        // short sNum = dNum; // No balance also.
+        short sNum = (short) dNum; // type casting
+        int numberValue = Integer.parseInt(number); //
+        System.out.println(numberValue + 6); // 1234 + 6 = 1240
+
+
     }
 }
