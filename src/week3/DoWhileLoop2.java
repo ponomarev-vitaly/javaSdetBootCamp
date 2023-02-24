24f8f47b2420c145442ea0d69e2f148036d28eca
ponomarev-vitaly <vp211082@gmail.com>
2023-02-24 05:10:45 +0300

    I added some new code for while loop.

--- src/week3/DoWhileLoop2.java
+++ src/week3/DoWhileLoop2.java
@@ -1,12 +1,28 @@
 package week3;
 
+import java.util.Scanner;
+
 public class DoWhileLoop2 {
     public static void main(String[] args) {
         Boolean passCode = false;
+        int doorCode = 2025;
+        int userCode = 0;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the passCode...");
+        passCode = scan.nextBoolean();
 
         while(passCode){
             System.out.println("Welcome to our company, the door will be opened in 2 seconds...");
-            System.out.println("The door will be opened now.");
+            System.out.println("The door opened now.");
+            passCode = false;
+        }
+
+        System.out.println("Please enter the door code: ");
+        userCode = scan.nextInt();
+        while(doorCode == userCode){
+            System.out.println("Welcome to our Company...");
+            break;
         }
     }
 }
