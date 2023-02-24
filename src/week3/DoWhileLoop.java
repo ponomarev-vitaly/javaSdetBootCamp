6022e6999bf5355b5ce9d95d24cf0ba01a268129
ponomarev-vitaly <vp211082@gmail.com>
2023-02-24 04:41:14 +0300

    I added an example of how to use while loop.

--- src/week3/DoWhileLoop.java
+++ src/week3/DoWhileLoop.java
@@ -6,8 +6,21 @@ public class DoWhileLoop {
         // statement
     }
 
-    do{ condition = true or false
+    do{ The code is going to be executed although the condition is true or false in the first time.
         // statement
-    }while (condition);
+        //
+        //
+    }while (condition); If the condition is true and do while loop will continue to execute.
      */
+
+    public static void main(String[] args) {
+        // if x < 10 add the x value to total variable. the initial value of the x = 4.
+        int x = 4;
+        int total = 0;
+        while(x<10){
+            total = x + total; // total += x
+            x++;
+        }
+        System.out.println("Total number: " + total);
+    }
 }
