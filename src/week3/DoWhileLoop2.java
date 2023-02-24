ef2702afe05f1cec204316b144bab33dc3a7f291
ponomarev-vitaly <vp211082@gmail.com>
2023-02-24 04:57:19 +0300

    I added a new sample of code for do while loop.

--- src/week3/DoWhileLoop2.java
+++ src/week3/DoWhileLoop2.java
@@ -1,4 +1,12 @@
 package week3;
 
 public class DoWhileLoop2 {
+    public static void main(String[] args) {
+        Boolean passCode = false;
+
+        while(passCode){
+            System.out.println("Welcome to our company, the door will be opened in 2 seconds...");
+            System.out.println("The door will be opened now.");
+        }
+    }
 }
