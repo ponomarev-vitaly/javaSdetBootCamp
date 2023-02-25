74baa7575d1c7c4f0b8d217459b0e9a7ff928600
ponomarev-vitaly <vp211082@gmail.com>
2023-02-25 04:00:32 +0300

    I added a new example of the ternary operator.

--- src/week3/TernaryOperator.java
+++ src/week3/TernaryOperator.java
@@ -7,8 +7,8 @@ public class TernaryOperator {
         // NESTED Ternary Operator : (expression1 ? (expression2 ? expression6 : expression7) : (expression3 ? expression4 : expression5))
         // condition_is_correct ? result1 : accept_result2
 
-        int a = 4;
-        if(a>3) {
+        int a = 3;
+        if(a > 3) {
             System.out.println("A is greater");
         } else {
             System.out.println("A is smaller");
@@ -18,6 +18,11 @@ public class TernaryOperator {
         result = a > 3 ? "A is greater" : "A is smaller";
         System.out.println(result);
         System.out.println(a > 3 ? "A is greater" : "A is smaller");
+
+        // Math >= 45 and math < 60 --> MathGrade : D("Your grade is D", otherwise "We didn't find your grade")
+        int math = 65;
+        String mathGrade = (math >= 45 && math < 60) ? "Your grade is D" : "We didn't find your grade";
+        System.out.println(mathGrade);
 
     }
 }
