5ba7d35b88510d516f059dbd1efb593e7cc9ccd8
ponomarev-vitaly <vp211082@gmail.com>
2023-02-25 05:16:41 +0300

    I added a new example of switch case with the days of the week.

--- src/week3/SwitchCase.java
+++ src/week3/SwitchCase.java
@@ -39,6 +39,35 @@ public class SwitchCase {
                 System.out.println("Please enter the valid name.");
         }
 
+        System.out.println("Please enter the current day: ");
+        String currentDay = scan.nextLine();
+
+        switch(currentDay) {
+            case "Monday":
+                System.out.println("Today is Monday.");
+                break;
+            case "Tuesday":
+                System.out.println("Today is Tuesday.");
+                break;
+            case "Wednesday":
+                System.out.println("Today is Wednesday.");
+                break;
+            case "Thursday":
+                System.out.println("Today is Thursday.");
+                break;
+            case "Friday":
+                System.out.println("Today is Friday.");
+                break;
+            case "Saturday":
+                System.out.println("Today is Saturday.");
+                break;
+            case "Sunday":
+                System.out.println("Today is Sunday.");
+                break;
+            default:
+                System.out.println("Please enter valid day.");
+        }
+
 
     }
 }
