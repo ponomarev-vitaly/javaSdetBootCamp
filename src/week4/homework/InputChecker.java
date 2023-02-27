e159cf42ad865f85ecbc28abb4b834b7c75be1c9
ponomarev-vitaly <vp211082@gmail.com>
2023-02-28 01:56:09 +0300

    I added new class to check the user's numbers whether they equal zero or no.

---
+++ src/week4/homework/InputChecker.java
@@ -0,0 +1,21 @@
+package week4.homework;
+
+import java.util.Scanner;
+
+public class InputChecker {
+    public static void main(String[] args) {
+        int i=1;
+        for(i=1;i<=10;i++){ // For loop for the checking of the numbers for 10 times.
+            System.out.println("Please enter the number you want to check: "); // Give directions to the user.
+            Scanner scan = new Scanner(System.in); // Create new scanner class object.
+            int number = scan.nextInt();
+
+                if(number != 0) {
+                    System.out.println("You entered the number, which is not zero: " + number);
+                } else{
+                    System.out.println("You entered zero and the program will stop right now");
+                    break;
+                }
+        }
+    }
+}
