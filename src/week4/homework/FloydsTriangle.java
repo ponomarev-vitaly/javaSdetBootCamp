fcb52772d26646f22141dee083a6462fc55c8e38
ponomarev-vitaly <vp211082@gmail.com>
2023-02-28 02:13:23 +0300

    I added some code to print the Floyd's triangle. Have to check this code later.

--- src/week4/homework/FloydsTriangle.java
+++ src/week4/homework/FloydsTriangle.java
@@ -12,7 +12,7 @@ public class FloydsTriangle {
     public static void main(String[] args) {
         int rows = 4;
         for(int i = 0; i<= rows; i++){ // This for loop to display the rows.
-            for(int number = 0; number <= i; number++){ // This for loop to display the numbers.
+            for(int number = i; number < (2*i+1); number++){ // This for loop to display the numbers.
 
                 System.out.print((number + 1) + " ");
             }
