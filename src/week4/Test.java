ce0e01854127b7c684ac3a32325640431afba966
ponomarev-vitaly <vp211082@gmail.com>
2023-02-28 07:32:09 +0300

    I fixed an issue by changing the structure from the wrong to the correct one.

--- src/week4/Test.java
+++ src/week4/Test.java
@@ -3,26 +3,25 @@ package week4;
 public class Test {
     static int i;
     int j;
+
     // static block:
-    static{
+    static {
         i = 10;
         System.out.println("Static block called ");
     }
-    Test(){
+
+    Test() {
         System.out.println("Constructor called ");
     }
 
-    public static void main(String[] args) {
-    // We created inner main class.
-    class Main{
+
+}
+    class Main {
         public static void main(String[] args) {
             // Although we have two objects, static block is executed only once.
             Test t1 = new Test();
             Test t2 = new Test();
-
-
         }
+    }
 
-    }
-    }
-}
+
