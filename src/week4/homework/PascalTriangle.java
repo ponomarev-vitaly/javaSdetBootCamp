86a89ef7930cbb6e50d23d0b979f4924557228b6
ponomarev-vitaly <vp211082@gmail.com>
2023-03-01 01:35:28 +0300

    I have new class to display the Pascal's triangle according to the task.

---
+++ src/week4/homework/PascalTriangle.java
@@ -0,0 +1,16 @@
+package week4.homework;
+
+/**
+ * Print Pascal’s triangle
+ * result should be like this;
+ *            1
+ *          1   1
+ *        1   2   1
+ *      1   3   3    1
+ *    1   4   6   4   1
+ *  1   5   10   10  5   1
+ */
+public class PascalTriangle {
+
+
+}
