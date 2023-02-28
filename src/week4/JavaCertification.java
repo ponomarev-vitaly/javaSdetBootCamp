1638a9e3a1f54fb3d30b820dae42deaddfd05f2a
ponomarev-vitaly <vp211082@gmail.com>
2023-02-28 03:08:39 +0300

    I added new class to check the tests from the JAVA certification.

---
+++ src/week4/JavaCertification.java
@@ -0,0 +1,14 @@
+package week4;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class JavaCertification {
+    public static void main(String[] args) {
+        String date = LocalDate
+                .parse("2014-05-04")
+                .format(DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(date);
+
+    }
+}
