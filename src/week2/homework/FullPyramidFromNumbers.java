680ef9ace24ef1b465f95d9ffdd4c64f83824673
ponomarev-vitaly <vp211082@gmail.com>
2023-03-01 01:11:38 +0300

    I have added a piece of code ro display the row of numbers when they decrease.

--- src/week2/homework/FullPyramidFromNumbers.java
+++ src/week2/homework/FullPyramidFromNumbers.java
@@ -17,32 +17,25 @@ public class FullPyramidFromNumbers {
         Scanner scan = new Scanner(System.in);
         System.out.printf("Please enter the number of rows you want to display: ");
         int rows = scan.nextInt();
-        int i, k, j, n, counter = 1;
+        int i, k, j, n, counter = 0;
 
         for (i = 0; i < rows; i++){ // Creating rows.
 
             for (k = ((rows - i)*2-1); k > 1; k--) { // This loop displays " "
                 System.out.print(" ");
             }
-            int x = 0;
+
 
             for (j = i; j < (2*i+1); j++) { // This loop displays numbers when they increase from theirs minimum.
                 System.out.print((j + 1) + " ");
-                x = j;
-                while(x == 2 * i + 1){
-                    System.out.println(j-1);
-                    if (j == i +1) {
-                        x--;
-                    }
-                    j--;
-                }
-            }
-// This part of code doesn't work properly.
-            for (n = i ; n < ((counter-1)+i); n++) { // This loop displays numbers when they decrease from theirs maximum.
-                System.out.print(((counter-1)+i) + " ");
 
             }
-            counter++;
+// This part of code doesn't work properly.
+            for (n = counter ; n >= counter - i + 1; n--) { // This loop displays numbers when they decrease from theirs maximum.
+                System.out.print(n + " ");
+
+            }
+            counter+=2;
 
             System.out.println();
         }
