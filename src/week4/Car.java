842a16006e2becfe8d83dd7a6878a7f46c6be6ff
ponomarev-vitaly <vp211082@gmail.com>
2023-02-28 04:21:53 +0300

    I added new Car class to demonstrate how constructors work.

---
+++ src/week4/Car.java
@@ -0,0 +1,12 @@
+package week4;
+public class Car { // We created the Car class.
+    int price;
+    String make;
+    String model;
+
+    public Car(int price, String make, String model) { // We created the constructor.
+        this.price = price;
+        this.make = make;
+        this.model = model;
+    }
+}
