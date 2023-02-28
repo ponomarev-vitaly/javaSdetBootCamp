b249f6f068a55adfcc0fcf8b2f7fcd4ff789dacb
ponomarev-vitaly <vp211082@gmail.com>
2023-02-28 16:04:22 +0300

    I added 3 possible solutions from the ChatGPT, but all of them are not working properly.

--- src/week2/homework/FullPyramidFromNumbersThirdSolution.java
+++ src/week2/homework/FullPyramidFromNumbersThirdSolution.java
@@ -10,28 +10,72 @@ package week2.homework;
  * 5 6 7 8 9 8 7 6 5
  */
 public class FullPyramidFromNumbersThirdSolution {
+// This code is not working properly.
+//        public static void main(String[] args) {
+//            int rows = 5;
+//            int number = 1;
+//
+//            for (int i = 1; i <= rows; i++) {
+//                for (int j = 1; j <= rows - i; j++) {
+//                    System.out.print("  "); // print spaces for indentation
+//                }
+//
+//                for (int k = 1; k <= i; k++) {
+//                    System.out.print(number + " ");
+//                    number++;
+//                }
+//
+//                for (int l = i + 1; l <= 2 * i - 1; l++) {
+//                    System.out.print(number - 2 * i + l + " ");
+//                }
+//
+//                System.out.println(); // move to next line
+//            }
+//        }
+// This code is not working properly.
+//    public static void main(String[] args) {
+//        int rows = 5;
+//        int number = 1;
+//
+//        for (int i = 1; i <= rows; i++) {
+//            for (int j = 1; j <= rows - i; j++) {
+//                System.out.print("  "); // print spaces for indentation
+//            }
+//
+//            for (int k = 1; k <= i; k++) {
+//                System.out.print(number + " ");
+//                number++;
+//            }
+//
+//            for (int l = i + 1; l <= 2 * i - 1; l++) {
+//                System.out.print(number - 2 * rows + l + " ");
+//            }
+//
+//            System.out.println(); // move to next line
+//        }
+//    }
+    public static void main(String[] args) {
+        int rows = 5;
+        int number = 1;
 
-        public static void main(String[] args) {
-            int rows = 5;
-            int number = 1;
-
-            for (int i = 1; i <= rows; i++) {
-                for (int j = 1; j <= (rows - i); j++) {
-                    System.out.print("  "); // print spaces for indentation
-                }
-
-                for (int k = 1; k <= i; k++) {
-                    System.out.print(number + " ");
-                    number++;
-                }
-
-                for (int l = i + 1; l <= 2 * i - 1; l++) {
-                    System.out.print((number - 2 * i + l) + " ");
-                }
-
-                System.out.println(); // move to next line
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  "); // print spaces for indentation
             }
+
+            for (int k = i; k < i + i; k++) {
+                System.out.print(number + " ");
+                if (k < rows + i - 1) {
+                    number++;
+                } else {
+                    number--;
+                }
+            }
+
+            System.out.println(); // move to next line
+            number = i + 1; // reset number for next row
         }
     }
+}
 
 
