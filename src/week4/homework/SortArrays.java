6a3680b6bf826145bf07a62dad7f5ac07db2226a
ponomarev-vitaly <vp211082@gmail.com>
2023-03-02 01:08:37 +0300

    I added new class for a Java program to sort a numeric array and a string array.

---
+++ src/week4/homework/SortArrays.java
@@ -0,0 +1,10 @@
+package week4.homework;
+
+/**
+ * Write a Java program to sort a numeric array and a string array.
+ */
+public class SortArrays {
+    public static void main(String[] args) {
+
+    }
+}
