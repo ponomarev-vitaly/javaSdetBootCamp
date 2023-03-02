a9eeb73b0a4290fc389b19c433b0d7f96a1e6ec3
ponomarev-vitaly <vp211082@gmail.com>
2023-03-02 04:21:48 +0300

    I created new method to check the specific number in the array.

--- src/week4/homework/TestSpecificValueInArray.java
+++ src/week4/homework/TestSpecificValueInArray.java
@@ -1,10 +1,22 @@
 package week4.homework;
-
 /**
  * 4. Write a Java program to test if an array contains a specific value.
  */
 public class TestSpecificValueInArray {
     public static void main(String[] args) {
-
+        int numberArr[] = {344, 555, 66, 88, 999, 66, 55};
+        int number = 100;
+        System.out.println(contains(numberArr, number));
+        boolean check = contains(numberArr, number);
+        System.out.println(check);
+        // contains(numberArr, 99); // Call for the contains method.
+    }
+    // Create a method contains.
+    public static boolean contains(int arr[], int specificNumber){
+        boolean result = false;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == specificNumber) result = true;
+        }
+        return result;
     }
 }
