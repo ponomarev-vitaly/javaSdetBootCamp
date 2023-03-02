97f4aa864e8de72f553e573b41b37671a57e8f34
ponomarev-vitaly <vp211082@gmail.com>
2023-03-02 05:09:01 +0300

    I added sort method to sort the array. I added output to output the index of the array.

--- src/week4/BinarySearch.java
+++ src/week4/BinarySearch.java
@@ -1,5 +1,16 @@
 package week4;
 
-public class BinarySearch {
+import java.util.Arrays;
 
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        int numbers[] = {1, 2, 4, 46, 8, 14, 7};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.binarySearch(numbers, 7));
+        // If there is no such element in the array the result will be -1.
+        // If yes - 0.
+
+
+    }
 }
