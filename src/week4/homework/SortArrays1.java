48c8208b3e01cba653613b6f02c659cfc7915564
ponomarev-vitaly <vp211082@gmail.com>
2023-03-02 03:13:34 +0300

    I added for loop to sort elements in an ascending way. I printed out the new array

--- src/week4/homework/SortArrays1.java
+++ src/week4/homework/SortArrays1.java
@@ -1,7 +1,36 @@
 package week4.homework;
+
+import week3.MyClass;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Write a Java program to sort a numeric array and a string array.
  */
 public class SortArrays1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the number of elements in the array: "); // Give directions for the user.
+        int i = scan.nextInt();
+        int[] myArray = new int[i];
+        Random rd = new Random(); // Create Random object.
+        for(int j = 0; j<myArray.length; j++){
+            myArray[j] = rd.nextInt(); // Implementing random integers in an array.
+        }
+        System.out.println(Arrays.toString(myArray)); // This is just to print out what we've got for the initial array.
 
+        int minNumber = 0; // Firstly let's sort it in an ascending way.
+        for(int n = 0; n < myArray.length; n++){
+            for(int m = n + 1; m < myArray.length; m++) {
+                if (myArray[n] > myArray[m]) {
+                    minNumber = myArray[n];
+                    myArray[n] = myArray[m];
+                    myArray[m] = minNumber;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(myArray));
+    }
 }
