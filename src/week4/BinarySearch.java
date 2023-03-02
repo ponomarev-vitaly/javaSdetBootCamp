92bf6240f0647181135033a51afeb9495a384a86
ponomarev-vitaly <vp211082@gmail.com>
2023-03-02 05:25:10 +0300

    I added some code to check the value is in the array.

--- src/week4/BinarySearch.java
+++ src/week4/BinarySearch.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 public class BinarySearch {
     public static void main(String[] args) {
 
-        int numbers[] = {1, 2, 4, 46, 8, 14, 7};
+        int numbers[] = {2, 4, 46, 8, 14, 7};
         Arrays.sort(numbers);
         System.out.println(Arrays.binarySearch(numbers, 7));
         // If there is no such element in the array the result will be -1.
         // If yes - 0.
 
-
+        System.out.println(Arrays.binarySearch(numbers, 1));
+        System.out.println(Arrays.binarySearch(numbers, 3));
+        System.out.println(Arrays.binarySearch(numbers, 55));
     }
 }
