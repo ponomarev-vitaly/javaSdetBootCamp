b4de3563dc448351a00295f2320eb6639dbc1a52
ponomarev-vitaly <vp211082@gmail.com>
2023-03-02 03:40:35 +0300

    I applied the method to calculate the average of the array.

--- src/week4/homework/AverageArray.java
+++ src/week4/homework/AverageArray.java
@@ -1,10 +1,18 @@
 package week4.homework;
 
+import java.util.OptionalDouble;
+
+import static java.util.Arrays.*;
+
 /**
  * Write a Java program to calculate the average value of array elements.
  */
 public class AverageArray {
-    double averageOfTheArray = 0;
-    double[] numericArray = {11.6,25.0,55555.3,45645654.9,1.0,33.33}; // Create an array.
+    public static void main(String[] args) {
 
+        // double averageOfTheArray;
+        double[] numericArray = {11.6, 25.0, 55555.3, 45645654.9, 1.0, 33.33}; // Create an array.
+        OptionalDouble averageOfTheArray = stream(numericArray).average(); // Try to apply an average method to an array in order ot calculate the average.
+        System.out.printf("The average of the array's elements is: " + averageOfTheArray); // Print the average of the array.
+    }
 }
