93055e14d0f9807b2d1d1a76cd9a19bca8f9d242
ponomarev-vitaly <vp211082@gmail.com>
2023-03-02 03:52:22 +0300

    I added one more solution to calculate the average of the array.

--- src/week4/homework/AverageArray.java
+++ src/week4/homework/AverageArray.java
@@ -14,5 +14,13 @@ public class AverageArray {
         double[] numericArray = {11.6, 25.0, 55555.3, 45645654.9, 1.0, 33.33}; // Create an array.
         OptionalDouble averageOfTheArray = stream(numericArray).average(); // Try to apply an average method to an array in order ot calculate the average.
         System.out.printf("The average of the array's elements is: " + averageOfTheArray); // Print the average of the array.
+        System.out.println();
+        // Here is another solution for this task.
+        double sum = 0;
+        for(int i = 0; i < numericArray.length; i++){
+            sum += numericArray[i];
+        }
+        double averageOfTheArray1 = sum / numericArray.length; // Apply the simple arithmetic solution to find the average of the array.
+        System.out.printf("The average of the array's elements is: " + averageOfTheArray1);
     }
 }
