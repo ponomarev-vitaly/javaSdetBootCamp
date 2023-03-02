76c100d62397f4497fe969520530038cdd2e0446
ponomarev-vitaly <vp211082@gmail.com>
2023-03-02 03:54:17 +0300

    I added new class to test if in array there is a specific value.

---
+++ src/week4/homework/TestSpecificValueInArray.java
@@ -0,0 +1,10 @@
+package week4.homework;
+
+/**
+ * 4. Write a Java program to test if an array contains a specific value.
+ */
+public class TestSpecificValueInArray {
+    public static void main(String[] args) {
+
+    }
+}
