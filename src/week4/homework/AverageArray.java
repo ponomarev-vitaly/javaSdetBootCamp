1a451d8fcd22f22f7cbb4da2b7a035fe9140cacf
ponomarev-vitaly <vp211082@gmail.com>
2023-03-02 03:31:08 +0300

    I initialized new variable for the average of the array.

--- src/week4/homework/AverageArray.java
+++ src/week4/homework/AverageArray.java
@@ -4,5 +4,7 @@ package week4.homework;
  * Write a Java program to calculate the average value of array elements.
  */
 public class AverageArray {
+    double averageOfTheArray = 0;
+    double[] numericArray = {11.6,25.0,55555.3,45645654.9,1.0,33.33}; // Create an array.
 
 }
