8b752e7aa7ce0413f0b57af5042ceaabc3c1a9a7
ponomarev-vitaly <vp211082@gmail.com>
2023-03-03 07:07:02 +0300

    I added String Buffer methods.

--- src/week4/StringMethods.java
+++ src/week4/StringMethods.java
@@ -59,5 +59,30 @@ public class StringMethods {
         String name = new String("Java String.");
         System.out.println(name);
 
+        // append() method
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.append("Java");
+        System.out.println(sb);
+
+        // insert() method
+        StringBuffer sb1 = new StringBuffer("Hello ");
+        sb1.insert(3, "Java"); // Now original string is changed.
+        System.out.println(sb1);
+
+        // replace() method
+        StringBuffer sb2 = new StringBuffer("Hello");
+        sb2.replace(1,3,"Java");
+        System.out.println(sb2);
+
+        // delete() method
+        StringBuffer sb3 = new StringBuffer("Hello");
+        sb3.delete(1,3);
+        System.out.println(sb3);
+
+        // reverse() method
+        StringBuffer sb4 = new StringBuffer("Hello");
+        sb4.reverse();
+        System.out.println(sb4);
+
     }
 }
