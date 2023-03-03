a933507e40a1d5ac308bf7b79d2897bf033e626a
ponomarev-vitaly <vp211082@gmail.com>
2023-03-04 02:56:59 +0300

    I added new class to compare two strings lexicographically.

---
+++ src/week4/homework/CompareTwoStrings.java
@@ -0,0 +1,19 @@
+package week4.homework;
+
+/**
+ * 2.Write a Java program to compare two strings lexicographically. Two strings are lexicographically equal if they are the same length and contain the same characters in the same positions.
+ *
+ * Sample Output:
+ * String 1: This is Exercise 1
+ * String 2: This is Exercise 2
+ * "This is Exercise 1" is less than "This is Exercise 2"
+ */
+public class CompareTwoStrings {
+    public static void main(String[] args) {
+        String str1 = "This is Exercise 1";
+        String str2 = "This is Exercise 2";
+
+
+    }
+}
+
