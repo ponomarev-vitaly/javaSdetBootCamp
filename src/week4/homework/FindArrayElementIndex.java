d3dbba5f9f03a05c40980420c58dfffac43ec561
ponomarev-vitaly <vp211082@gmail.com>
2023-03-03 03:08:31 +0300

    I added new method to find the index of the array element.

--- src/week4/homework/FindArrayElementIndex.java
+++ src/week4/homework/FindArrayElementIndex.java
@@ -4,5 +4,20 @@ package week4.homework;
  * 5. Write a Java program to find the index of an array element.
  */
 public class FindArrayElementIndex {
+    public static void main(String[] args) {
+        int numberArr[] = {344, 555, 66, 88, 999, 66, 55};
+        int number = 88;
+        System.out.println(index(numberArr, number));
+        int j = index(numberArr, number);
+        System.out.println(j);
+    }
+
+    public static int index(int arr[], int specificNumber){
+        int index = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == specificNumber) index = arr[i];
+        }
+        return index;
+    }
 
 }
