ca12b0a169f3304a434b04d6b952055791c3669a
ponomarev-vitaly <vp211082@gmail.com>
2023-03-03 05:10:36 +0300

    I added new class to demonstrate how string methods work.

---
+++ src/week4/StringMethods.java
@@ -0,0 +1,13 @@
+package week4;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String string1 = "Java";
+        String string2 = "Python";
+        String string3 = "Java Script";
+
+        // Length of String > .length
+        int stringLength = string1.length();
+        System.out.println("string1 length is: " + stringLength);
+    }
+}
