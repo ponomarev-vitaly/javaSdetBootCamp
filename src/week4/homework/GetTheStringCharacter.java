1fc87029a575b8f7824b4fb14cd0d2acb8d4e9b2
ponomarev-vitaly <vp211082@gmail.com>
2023-03-04 02:52:29 +0300

    I added some code to find the character at the given index within the String.

--- src/week4/homework/GetTheStringCharacter.java
+++ src/week4/homework/GetTheStringCharacter.java
@@ -1,5 +1,9 @@
 package week4.homework;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 /**
  * 1. Write a Java program to get the character at the given index within the String.
  * Sample Output:
@@ -8,6 +12,18 @@ package week4.homework;
  * The character at position 10 is i
  */
 public class GetTheStringCharacter {
+    public static void main(String[] args) {
+        String originalStr = "Java Exercises!";
+        System.out.println("Enter the index within the String: ");
+        Scanner scan = new Scanner(System.in);
+        int index = scan.nextInt();
+        System.out.println("The character at position " + index + " is " + originalStr.charAt(index));
 
-
+//        for(int i = index; i < originalStr.length(); i++){
+//            if(index <= originalStr.length()) {
+//                if (i == originalStr.indexOf(i))
+//                    System.out.println("The character at position " + index + " is " + originalStr.charAt(i));
+//            }else System.out.println("You entered the index out of the range!");
+//        }
+    }
 }
