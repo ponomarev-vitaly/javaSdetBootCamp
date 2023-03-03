fae41be8c4b6c672f9d7701e759f8c93fb8ecbf3
ponomarev-vitaly <vp211082@gmail.com>
2023-03-04 02:31:28 +0300

    I added new class to get the character at the given index within the String.

---
+++ src/week4/homework/GetTheStringCharacter.java
@@ -0,0 +1,13 @@
+package week4.homework;
+
+/**
+ * 1. Write a Java program to get the character at the given index within the String.
+ * Sample Output:
+ * Original String = Java Exercises!
+ * The character at position 0 is J
+ * The character at position 10 is i
+ */
+public class GetTheStringCharacter {
+
+
+}
