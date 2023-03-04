29c585fb4a0aa23629993ca0b79a3f0573e11697
ponomarev-vitaly <vp211082@gmail.com>
2023-03-04 06:37:43 +0300

    I added some code for the recursion.

--- src/week4/Recursion.java
+++ src/week4/Recursion.java
@@ -1,4 +1,12 @@
 package week4;
 
 public class Recursion {
+    public static void main(String[] args) {
+
+    }
+    static int factorial(int n){
+        if(n!=0) // termination condition
+            return n * factorial(n-1);
+        else return 1;
+    }
 }
