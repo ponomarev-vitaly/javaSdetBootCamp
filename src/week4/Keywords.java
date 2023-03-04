11ceb288cb5db677afac306ad0963e154bff2ce8
ponomarev-vitaly <vp211082@gmail.com>
2023-03-04 05:54:57 +0300

    I added an example of the final keyword.

--- src/week4/Keywords.java
+++ src/week4/Keywords.java
@@ -1,4 +1,12 @@
 package week4;
 
 public class Keywords {
+    public static void main(String[] args) {
+        // create a final integer variable
+        final int age = 33;
+
+        // try to change the final variable
+//        age = 45; // Age variable can not be changed in such case.
+        System.out.println(age);
+    }
 }
