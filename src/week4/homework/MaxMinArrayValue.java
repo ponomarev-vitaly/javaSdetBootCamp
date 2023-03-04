1ac545dc33805979b11bd5647312948cdf191e8a
ponomarev-vitaly <vp211082@gmail.com>
2023-03-05 01:21:25 +0300

    I added new class for arrays comparison.

---
+++ src/week4/homework/MaxMinArrayValue.java
@@ -0,0 +1,15 @@
+package week4.homework;
+/**
+ * 9. Write a Java program to find the maximum and minimum value of an array.
+ */
+public class MaxMinArrayValue {
+    public static void main(String[] args) {
+        int arr[] = {10, 0, 30, 2, 7, 5, 90, 76, 100, 45, 55};
+
+
+        // For String arrays.
+        // String arr[] = {"orange", "red", "yellow", "blue", "white", "black", "green", "brown"};
+
+
+    }
+}
