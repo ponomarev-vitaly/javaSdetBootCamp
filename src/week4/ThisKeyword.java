998510c04fd6121f8a550d3b174d20b352b50ebf
ponomarev-vitaly <vp211082@gmail.com>
2023-03-04 06:22:23 +0300

    I added the demonstration for the ThisKeyword.

--- src/week4/ThisKeyword.java
+++ src/week4/ThisKeyword.java
@@ -1,5 +1,22 @@
 package week4;
 
 public class ThisKeyword {
+    String name;
 
+        // setter method
+        void setname(String name){
+            this.name = name;
+        }
+        // getter method
+        String getName(){
+            return this.name;
+        };
+
+    public static void main(String[] args){
+        ThisKeyword obj = new ThisKeyword();
+
+        // calling the getter and setter method
+        obj.setname("Adam");
+        System.out.println("Obj.name: " + obj.getName());
+    }
 }
