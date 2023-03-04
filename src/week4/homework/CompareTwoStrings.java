ab29c49d0c589af08ea0f5bc5fd8ce107f602e84
ponomarev-vitaly <vp211082@gmail.com>
2023-03-04 07:05:55 +0300

    I added compareTo method and it worked well this time. Code must be checked later.

--- src/week4/homework/CompareTwoStrings.java
+++ src/week4/homework/CompareTwoStrings.java
@@ -12,15 +12,18 @@ public class CompareTwoStrings {
     public static void main(String[] args) {
         String str1 = "This is Exercise 1";
         String str2 = "This is Exercise 2";
-        if(str1.length()==str2.length()){
-            // Add new check-up for the characters and positions.
-        } else if(str1.length()<str2.length()){
-            System.out.println("The length of the " + str1 + " is less than " + str2);
-        } else if (str1.length()>str2.length()){
-            System.out.println("The length of the " + str2 + " is less than " + str1);
-        };
-
-
+//        if(str1.length()==str2.length()){
+//            // Add new check-up for the characters and positions.
+//        } else if(str1.length()<str2.length()){
+//            System.out.println("The length of the " + str1 + " is less than " + str2);
+//        } else if (str1.length()>str2.length()){
+//            System.out.println("The length of the " + str2 + " is less than " + str1);
+//        };
+        int result = str1.compareTo(str2);
+        if(result<0) System.out.println("Str1 is less than str2.");
+        else if(result == 0) System.out.println("Str1 and str2 are equals.");
+        else // if result > 0
+            System.out.println("Str1 is greater than str2.");
     }
 }
 
