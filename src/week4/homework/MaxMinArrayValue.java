5268e57190082f9b0a3aba92f7c12d5c3ae8e5d6
ponomarev-vitaly <vp211082@gmail.com>
2023-03-05 01:34:38 +0300

    I added code to check min and max of the array.

--- src/week4/homework/MaxMinArrayValue.java
+++ src/week4/homework/MaxMinArrayValue.java
@@ -5,11 +5,24 @@ package week4.homework;
 public class MaxMinArrayValue {
     public static void main(String[] args) {
         int arr[] = {10, 0, 30, 2, 7, 5, 90, 76, 100, 45, 55};
+        int minArrayValue = 0;
+        int maxArrayValue = 0;
+
 
 
         // For String arrays.
         // String arr[] = {"orange", "red", "yellow", "blue", "white", "black", "green", "brown"};
 
 
+    }
+
+    public static void arrayValueComparison(int arr[], int minValue, int maxValue){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]<arr[i+1]){
+                minValue = arr[i];
+                maxValue = arr[i+1];
+            }
+        }
+        return; // Write values which must be returned.
     }
 }
