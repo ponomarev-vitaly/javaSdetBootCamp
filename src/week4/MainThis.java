7212c276e101c8cfacc5a28e7cd8e3a1f1cd0f32
ponomarev-vitaly <vp211082@gmail.com>
2023-03-04 06:09:29 +0300

    I added some new code for the demonstration of the object and the constructor.

--- src/week4/MainThis.java
+++ src/week4/MainThis.java
@@ -3,13 +3,19 @@ package week4;
 public class MainThis {
     int instVar;
 
-    MainThis (int instVar){
-        this.instVar = instVar;
-        System.out.println("This reference: " + this);
+//    MainThis (int instVar){ // In the constructor we have paremeter.
+//        this.instVar = instVar;
+//        System.out.println("This reference: " + this.instVar);
+//    }
+    MainThis(int instVar){
+        instVar = instVar;
+    }
+    MainThis(){ // Create default constructor.
+
     }
 
     public static void main(String[] args){
-        MainThis obj = new MainThis(8);
-        System.out.println("Object reference: " + obj);
+        MainThis obj = new MainThis(15); // We assigned the value.
+        System.out.println("Object reference: " + obj.instVar);
     }
 }
