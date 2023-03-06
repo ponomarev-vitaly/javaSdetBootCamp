832f12f6975cf51d56f3b36f0c6c6195fa60e578
ponomarev-vitaly <vp211082@gmail.com>
2023-03-07 02:13:25 +0300

    I added new class to compare a given string to the specified string buffer.

---
+++ src/week4/homework/CompareStringBuffer.java
@@ -0,0 +1,13 @@
+package week4.homework;
+
+/**
+ * 5. Write a Java program to compare a given string to the specified string buffer.
+ * Sample Output:
+ * Comparing example.com and example.com: true
+ * Comparing Example.com and example.com: false
+ */
+public class CompareStringBuffer {
+    public static void main(String[] args) {
+
+    }
+}
