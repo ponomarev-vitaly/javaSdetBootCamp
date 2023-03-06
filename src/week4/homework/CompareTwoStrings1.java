3c8f102d110835d6202b56e0774c2dc1c96259e1
ponomarev-vitaly <vp211082@gmail.com>
2023-03-07 02:01:05 +0300

    I applied two String methods to ignore case difeerencies and compare two strings.

--- src/week4/homework/CompareTwoStrings1.java
+++ src/week4/homework/CompareTwoStrings1.java
@@ -1,5 +1,23 @@
 package week4.homework;
 
+/**
+ * 3. Write a Java program to compare two strings lexicographically, ignoring case differences.
+ * Sample Output:
+ * String 1: This is exercise 1
+ * String 2: This is Exercise 1
+ * "This is exercise 1" is equal to "This is Exercise 1"
+ */
 public class CompareTwoStrings1 {
+    public static void main(String[] args) {
 
+        String str1 = "This is exercise 1";
+        String str2 = "This is Exercise 1";
+        String strOneModified = str1.toLowerCase();
+        String strTwoModified = str2.toLowerCase();
+        int result = strOneModified.compareTo(strTwoModified);
+        if (result < 0) System.out.println("Str1 is less than str2.");
+        else if (result == 0) System.out.println("Str1 and str2 are equals.");
+        else // if result > 0
+            System.out.println("Str1 is greater than str2.");
+    }
 }
