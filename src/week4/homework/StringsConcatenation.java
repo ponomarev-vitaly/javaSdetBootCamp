8e33c4929fe66a61231ae3c5e94a3442dd38fe30
ponomarev-vitaly <vp211082@gmail.com>
2023-03-07 02:02:53 +0300

    I added new class to concatenate two strings.

---
+++ src/week4/homework/StringsConcatenation.java
@@ -0,0 +1,14 @@
+package week4.homework;
+
+/**
+ * 4. Write a Java program to concatenate a given string to the end of another string.
+ * Sample Output:
+ * String 1: PHP Exercises and
+ * String 2: Python Exercises
+ * The concatenated string: PHP Exercises and Python Exercises
+ */
+public class StringsConcatenation {
+    public static void main(String[] args) {
+
+    }
+}
