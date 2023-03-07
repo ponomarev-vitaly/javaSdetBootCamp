80dc592da9749289de76ce592cefa3310c136cdb
ponomarev-vitaly <vp211082@gmail.com>
2023-03-07 04:17:20 +0300

    I added new task to solve with the same method.

--- src/week4/homework/SpecifiedCharacterReplacement.java
+++ src/week4/homework/SpecifiedCharacterReplacement.java
@@ -12,6 +12,18 @@ public class SpecifiedCharacterReplacement {
         System.out.println(str);
         String str1 = str.replace("Programming Language.", "Script and Python");
         System.out.println(str1);
+        /**
+         * 12. Write a Java program to replace each substring of a given string that matches the given regular expression with the given replacement.
+         * Sample string : "The quick brown fox jumps over the lazy dog."
+         * In the above string replace all the fox with cat.
+         * Sample Output:
+         * Original string: The quick brown fox jumps over the lazy dog.
+         * New String: The quick brown cat jumps over the lazy dog.
+          */
+        String str2 = "The quick brown fox jumps over the lazy dog.";
+        System.out.println(str2);
+        String str3 = str2.replace("fox", "cat");
+        System.out.println(str3);
     }
 }
 
