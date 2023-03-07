b87bfb554f2709de38b3c5992376471b66803240
ponomarev-vitaly <vp211082@gmail.com>
2023-03-07 06:53:02 +0300

    I added new methods for the ArrayLists.

--- src/week5/ArraylistClass.java
+++ src/week5/ArraylistClass.java
@@ -1,6 +1,8 @@
 package week5;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ArraylistClass {
@@ -35,5 +37,41 @@ public class ArraylistClass {
         System.out.println(birds.remove("Robin")); //--> returns true
         System.out.println("After removing the \"Robin\" element: " + birds);
 
+        System.out.println("Removed the " + birds.remove(2) + " from the birds ArrayList which has index number 2.");
+
+        System.out.println(birds.remove(1)); // --> in we put 10, 15 or bigger numbers in remove method we will get an error (IndexOutOfBoundsException).
+        // size and set methods as below
+        System.out.println(birds.size());
+        birds.set(1, "Welcome to Java");
+        System.out.println(birds.get(0));
+
+        // contains() method
+        System.out.println(birds.size());
+        System.out.println(birds.contains("The Birth"));
+        System.out.println(birds.contains("Welcome to Java"));
+
+        ArrayList<String> arrList = new ArrayList<>();
+        arrList.add("Y");
+        arrList.add("A");
+        arrList.add("H"); //---> [M, O, z]
+        Collections.sort(arrList); // Sorting the arrList. This method does not have return type. It's void method.
+        System.out.println(arrList);
+
+        // equals method
+        ArrayList<String> arrList2 = new ArrayList<>();
+        arrList.add("a");
+        arrList2.add("b");
+        arrList.add("c");
+        arrList2.add("c");
+        System.out.println("The original arrayList(arrList): " + arrList);
+        System.out.println("The original arrayList(arrList2): " + arrList2);
+        System.out.println(arrList.equals(arrList2));
+        arrList2.set(0, "a");
+        arrList2.add(0, "Y");
+        arrList2.add(0, "H");
+        arrList2.add(0, "A");
+        System.out.println("The updated original arrayList(arrList): " + arrList);
+        System.out.println("The updated original arrayList(arrList2): " + arrList2);
+        System.out.println(arrList.equals(arrList2));
     }
 }
