8bdc0c0c0c87e75f227035d8700118096610bc66
ponomarev-vitaly <vp211082@gmail.com>
2023-03-07 04:06:40 +0300

    I added new class to get the index of all characters of the alphabet.

---
+++ src/week4/homework/IndexOfAlphabetCharacters.java
@@ -0,0 +1,48 @@
+package week4.homework;
+
+import java.util.Arrays;
+
+/**
+ * 10. Write a Java program to get the index of all the characters of the alphabet.
+ *
+ * Use ASCII code to iterate.
+ */
+public class IndexOfAlphabetCharacters {
+    public static void main(String[] args) {
+        String str1 = "Vitalii P";
+        String str = str1.toLowerCase();
+        int a = str.indexOf("a", 0);
+        int b = str.indexOf("b", 0);
+        int c = str.indexOf("c", 0);
+        int d = str.indexOf("d", 0);
+        int e = str.indexOf("e", 0);
+        int f = str.indexOf("f", 0);
+        int g = str.indexOf("g", 0);
+        int h = str.indexOf("h", 0);
+        int i = str.indexOf("i", 0);
+        int j = str.indexOf("j", 0);
+        int k = str.indexOf("k", 0);
+        int l = str.indexOf("l", 0);
+        int m = str.indexOf("m", 0);
+        int n = str.indexOf("n", 0);
+        int o = str.indexOf("o", 0);
+        int p = str.indexOf("p", 0);
+        int q = str.indexOf("q", 0);
+        int r = str.indexOf("r", 0);
+        int s = str.indexOf("s", 0);
+        int t = str.indexOf("t", 0);
+        int u = str.indexOf("u", 0);
+        int v = str.indexOf("v", 0);
+        int w = str.indexOf("w", 0);
+        int x = str.indexOf("x", 0);
+        int y = str.indexOf("y", 0);
+        int z = str.indexOf("z", 0);
+        // System.out.println(Arrays.toString(str.toCharArray()));
+        System.out.println("Vitalii P");
+        System.out.println("====================");
+        System.out.println(v + " " + i + " " + t + " " + a + " " + l + " " + i + " " + i + " " + p);
+       // System.out.println(z); // Write in the console that there is no such a character. In case the return of the index number is - 1.
+
+    }
+}
+
