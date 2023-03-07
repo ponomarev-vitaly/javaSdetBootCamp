7fe636938d03aa77b077ac6b7eaa95e30d3c0f6d
ponomarev-vitaly <vp211082@gmail.com>
2023-03-07 05:54:50 +0300

    I added .remove method and applied it to the ArrayList.

--- src/week5/ArraylistClass.java
+++ src/week5/ArraylistClass.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class ArraylistClass {
     public static void main(String[] args) {
+        // Declare the ArrayList as below.
         // The important side is right side.
         ArrayList<String> list1 = new ArrayList<>();
         ArrayList<Integer> list2 = new ArrayList<Integer>(); // We must use Wrapper class in such case. Or String. But! We can not use primitive data type.
@@ -15,6 +16,7 @@ public class ArraylistClass {
 
         System.out.println(list5);
 
+        // birds.add("Hawk"); ---> adding the element to ArrayList
         ArrayList<String> birds = new ArrayList<>();
         birds.add("Hawk");
         System.out.println(birds);
@@ -25,5 +27,13 @@ public class ArraylistClass {
         System.out.println(birds);
 
         // birds.addAll("White", "Green"); // How to write a lot of meanings in just one line of code using method .add?
+
+        // birds.remove() ---> Remove the element from ArrayList
+        // remove() method is boolean type that's why if there's no such element in the ArrayList it's gonna return false.
+        // If there's such an element the remove method can remove and return true.
+        System.out.println(birds.remove("Fox")); //--> returns false
+        System.out.println(birds.remove("Robin")); //--> returns true
+        System.out.println("After removing the \"Robin\" element: " + birds);
+
     }
 }
