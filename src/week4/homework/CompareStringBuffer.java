646b1be086a82c56728cad01f1fca855c8bcb0de
ponomarev-vitaly <vp211082@gmail.com>
2023-03-07 03:02:19 +0300

    I added some code to compare string to the String Buffer.

--- src/week4/homework/CompareStringBuffer.java
+++ src/week4/homework/CompareStringBuffer.java
@@ -1,5 +1,7 @@
 package week4.homework;
 
+import java.util.Scanner;
+
 /**
  * 5. Write a Java program to compare a given string to the specified string buffer.
  * Sample Output:
@@ -8,7 +10,31 @@ package week4.homework;
  */
 public class CompareStringBuffer {
     public static void main(String[] args) {
+        // Solution 1.
+        String str1 = "example.com";
+        StringBuffer sb = new StringBuffer("example.com");
+        // System.out.println(sb);
+        int result = str1.compareTo(String.valueOf(sb));
+        if (result < 0) System.out.println("Str1 is less than StringBuffer");
+        else if (result == 0) System.out.println("Str1 and StringBuffer are equals.");
+        else // if result > 0
+            System.out.println("Str1 is greater than StringBuffer");
 
+        // Solution 2.
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the meaning of the String you want to compare to the String Buffer: ");
+        String str2 = scan.nextLine();
+        int result1 = str2.compareTo(String.valueOf(sb));
+        if (result1 < 0) System.out.println("Str2 is less than StringBuffer");
+        else if (result == 0) System.out.println("Str2 and StringBuffer are equals.");
+        else // if result > 0
+            System.out.println("Str2 is greater than StringBuffer");
+        // System.out.println(str2);
 
+        // Solution 3.
     }
+
+//    public static void compareStrings(){
+//
+//    }
 }
