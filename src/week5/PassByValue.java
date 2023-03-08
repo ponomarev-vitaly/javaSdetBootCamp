eb4b8bab4d1b52f54a1831965cfa0b8aa64c4c0f
ponomarev-vitaly <vp211082@gmail.com>
2023-03-08 07:07:57 +0300

    I added new class to demonstrate how pass by value works.

--- src/week5/PassByValue.java
+++ src/week5/PassByValue.java
@@ -2,6 +2,19 @@ package week5;
 
 public class PassByValue {
     public static void main(String[] args) {
+        double price = 100;
+        discountForVeteran(price);
+        discountForSeniors(price);
+        System.out.println(price);
+    }
 
+    public static void discountForVeteran(double price){
+        price = price * 0.80; // That means make the discount 20 %
+        System.out.println("Discounted price for Veterans " + price);
+    }
+
+    public static void discountForSeniors(double priceSeniors){
+        priceSeniors = priceSeniors * 0.90; // That means make the discount 10 %
+        System.out.println("Discounted price for Seniors " + priceSeniors);
     }
 }
