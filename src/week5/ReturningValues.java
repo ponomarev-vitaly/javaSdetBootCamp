ebc7533fc3c02bc0981ee9825c5463f00618bcff
ponomarev-vitaly <vp211082@gmail.com>
2023-03-08 07:08:15 +0300

    I added new class to demonstrate how pass by reference works.

---
+++ src/week5/ReturningValues.java
@@ -0,0 +1,21 @@
+package week5;
+
+public class ReturningValues {
+    public static void main(String[] args) {
+        int number = 1; // 1
+        String letters = "abc"; // abc
+        number(number); // 1
+        letters = letters(letters); // abcd
+        System.out.println(number + letters);  // 1abcd
+    }
+
+    public static int number(int number){
+        number++;
+        return number;
+    }
+
+    public static String letters(String letters){
+        letters = letters + "d";
+        return letters;
+    }
+}
