65c1f5a935f0beede6bb5e6c4932e34db4f44ff3
ponomarev-vitaly <vp211082@gmail.com>
2023-03-09 04:54:49 +0300

    I added some more examples of working with the arraylist's methods.

--- src/week5/Numbers.java
+++ src/week5/Numbers.java
@@ -1,10 +1,25 @@
 package week5;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Numbers {
     public static void main(String[] args) {
         ArrayList<Integer> num = new ArrayList<Integer>();
+        num.add(99);
+        num.add(46);
+        num.add(62);
+        num.add(27);
+        num.add(357);
+        num.add(21);
+
+        // Sorting the arraylist in ascending order.
+        System.out.println("Before " + num);
+        Collections.sort(num);
+        System.out.println("After sorting the arraylist: " + num);
+
+        num.add(3, 100);
+        System.out.println(num);
 
     }
 }
