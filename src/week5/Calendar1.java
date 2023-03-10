586cca44950c1a278621eaee9e27161a6c299748
ponomarev-vitaly <vp211082@gmail.com>
2023-03-10 06:22:58 +0300

    I added new class to demonstrate how calendar works.

--- src/week5/Calendar1.java
+++ src/week5/Calendar1.java
@@ -1,5 +1,24 @@
 package week5;
 
-public class Calendar1 {
+import java.util.Calendar;
 
+/**
+ * Write Java program to create a Date object using the Calendar class.
+ */
+public class Calendar1 {
+    public static void main(String[] args) {
+        int year = 2023;
+        int month = 5;
+        int date = 15;
+
+        Calendar calendar = Calendar.getInstance();
+        // Sets the given calendar field value and the time value.
+        calendar.clear();
+        System.out.println();
+        calendar.set(Calendar.YEAR,year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DATE, date);
+        System.out.println(calendar.getTime());
+
+    }
 }
