a6bc820f3dc6b23f45666835fab1da47b84e49b2
ponomarev-vitaly <vp211082@gmail.com>
2023-03-10 07:11:05 +0300

    I added new class TimeZone to show a current time zone.

--- src/week5/TimeZone.java
+++ src/week5/TimeZone.java
@@ -1,4 +1,19 @@
 package week5;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class TimeZone {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        ZoneId currentZone= ZoneId.systemDefault();
+        ZoneId newZone = ZoneId.of("Europe/Paris");
+        ZonedDateTime zonedNow = ZonedDateTime.of(now, currentZone);
+        ZonedDateTime newZonedNow = zonedNow.withZoneSameInstant(newZone);
+        System.out.println("Local Date Time: " + now);
+        System.out.println("Current Time Zone: " + currentZone);
+        System.out.println("New Time Zone: " + newZone);
+        System.out.println("New Zoned Date Time: " + newZonedNow);
+    }
 }
