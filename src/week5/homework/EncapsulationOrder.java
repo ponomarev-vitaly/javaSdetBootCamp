6064c3daa57a03b861bce70c3955f81a606eb5b3
ponomarev-vitaly <vp211082@gmail.com>
2023-03-14 02:21:28 +0300

    I added new class for the encapsulation order.

---
+++ src/week5/homework/EncapsulationOrder.java
@@ -0,0 +1,10 @@
+package week5.homework;
+
+/**
+ * Encapsulation Practice class : Please create a class(EncapsulationOrder) and use the variables below for encapsulation practice class.
+ * Use the getter and setter methods.
+ * class name : EncapsulationOrder
+ * variables : customerName, OrderNumber, Orders, OrderPrice, orderPrice, changeMoney
+ */
+public class EncapsulationOrder {
+}
