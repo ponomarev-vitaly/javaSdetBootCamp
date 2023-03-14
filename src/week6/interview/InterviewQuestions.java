0228a3934cd2128ee099070726c78603b5be7f31
ponomarev-vitaly <vp211082@gmail.com>
2023-03-14 23:04:59 +0300

    I added new class to collect Java interview questions.

---
+++ src/week6/interview/InterviewQuestions.java
@@ -0,0 +1,7 @@
+package week6.interview;
+
+/**
+ * 1. String reverse. Write a method that will take one string as an argument and will return the reverse version of this string.
+ */
+public class InterviewQuestions {
+}
