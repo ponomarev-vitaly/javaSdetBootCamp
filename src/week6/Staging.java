74e54488c2334147774509cdc56cca10df38fc47
ponomarev-vitaly <vp211082@gmail.com>
2023-03-17 04:02:52 +0300

    I added Staging class.

---
+++ src/week6/Staging.java
@@ -0,0 +1,8 @@
+package week6;
+
+public class Staging {
+    public static void main(String[] args) {
+        System.out.println("This file is in my git and staging area.");
+        System.out.println("That's why I can commit this file and send it to local repository.");
+    }
+}
