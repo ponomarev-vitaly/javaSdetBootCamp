7126699968c7d7373e9d079bcdaeef4e8262d171
ponomarev-vitaly <vp211082@gmail.com>
2023-03-17 05:38:36 +0300

    I added new classes to demonstrate how git commands work.

---
+++ src/week6/NewGit.java
@@ -0,0 +1,10 @@
+package week6;
+
+public class NewGit {
+    public static void main(String[] args) {
+        System.out.println("This class is in my workspace.");
+        System.out.println("But it's not in my staging area.");
+        System.out.println("That's why I can not commit and send this file to staging area and local repository.");
+
+    }
+}
--- src/week6/Staging.java
+++ src/week6/Staging.java
@@ -2,7 +2,8 @@ package week6;
 
 public class Staging {
     public static void main(String[] args) {
-        System.out.println("This file is in my git and staging area.");
-        System.out.println("That's why I can commit this file and send it to local repository.");
+        System.out.println("This file is in my git and staging area."); // git add
+        System.out.println("That's why I can commit this file and send it to local repository."); // git commit
+        System.out.println("I can send it remote repository now."); // git push
     }
 }
