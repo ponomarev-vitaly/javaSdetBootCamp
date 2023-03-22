3160c98856d42758a3877feee64b3ce9cbb36f66
ponomarev-vitaly <vp211082@gmail.com>
2023-03-23 01:24:04 +0300

    I added class to test the question 40 from the Java certification block of questions.

---
+++ src/week7/homework/TestQuestion40.java
@@ -0,0 +1,17 @@
+package week7.homework;
+
+/**
+ * Question 40.
+ */
+public class TestQuestion40 {
+    public static void main(String[] args) {
+        int numbers[] = {12,13,42,32,15,156,23,51,12};
+        int[]keys = findMax(numbers);
+        // System.out.println(keys.toString());
+    }
+
+    public static int[] findMax(int[] numbers){
+        int[] keys = new int[3];
+        return keys;
+    }
+}
