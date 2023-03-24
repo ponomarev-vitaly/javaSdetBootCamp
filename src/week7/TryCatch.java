5ad7e7322ef654de415c03c866034f152526576c
ponomarev-vitaly <vp211082@gmail.com>
2023-03-24 06:37:28 +0300

    I added an example of TryCatch block.

--- src/week7/TryCatch.java
+++ src/week7/TryCatch.java
@@ -1,4 +1,16 @@
 package week7;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class TryCatch {
+    public static void main(String[] args) {
+        FileInputStream file = null;
+        try{
+            file = new FileInputStream("c:/textfile.txt");
+        }
+        catch(IOException e){
+            System.out.println("The specified file is not present at the given path.");
+        }
+    }
 }
