477d3f4fe5665a3953db401bcdae1dd3f4734e2a
ponomarev-vitaly <vp211082@gmail.com>
2023-03-25 02:51:38 +0300

    I added some examples of the exceptions and how to fix them.

--- src/week7/exceptions/ExceptionsDemo.java
+++ src/week7/exceptions/ExceptionsDemo.java
@@ -1,6 +1,11 @@
 package week7.exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     /*
@@ -14,6 +19,36 @@ public class ExceptionsDemo {
 
      */
     public static void show(){
-        var reader = new FileReader("file.txt"); // This is an example of Checked exception.
+        FileReader reader = null;
+
+        try {
+            // var reader = new FileReader("file.txt"); // This is an example of Checked exception.
+            var value = reader.read();
+            new SimpleDateFormat().parse("");
+            // reader.close();
+            // System.out.println("File opened");
+    // Multiple catch blocks.
+        } catch (FileNotFoundException e) {
+            // System.out.println(ex.getMessage()); // File does not exist
+            e.printStackTrace();
+        } catch (IOException e){
+            System.out.println("Could not read data.");
+        } catch (ParseException e) {
+            System.out.println("Could not read data.");
+        }
+//        finally {
+//            if(reader != null) {
+//                try {
+//                    reader.close();
+//                } catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                    // e.printStackTrace(e);
+//                }
+//            }
+//        }
+        // ...
+        // reader.close(); // Use finally block!
+
+        // System.out.println("File opened!");
     }
 }
