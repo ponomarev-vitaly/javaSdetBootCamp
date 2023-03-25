0a1cb96327eebf41ffa397b94effa0986d81f0b8
ponomarev-vitaly <vp211082@gmail.com>
2023-03-25 20:41:47 +0300

    Create a short form for the catch block exceptions.

--- src/week7/DoubleCatchBlock.java
+++ src/week7/DoubleCatchBlock.java
@@ -7,11 +7,16 @@ public class DoubleCatchBlock {
     public void writeList(){
         try{
             arr[10]=11; // We get this result in the console: IndexOutOfBoundsException => Index 10 out of bounds for length 10.
-        } catch (NumberFormatException eNF){
-            System.out.println("NumberFormatException => " + eNF.getMessage());
         }
-        catch (IndexOutOfBoundsException eIOB){
-            System.out.println("IndexOutOfBoundsException => " + eIOB.getMessage());
+//        catch (NumberFormatException eNF){
+//            System.out.println("NumberFormatException => " + eNF.getMessage());
+//        }
+//        catch (IndexOutOfBoundsException eIOB){
+//            System.out.println("IndexOutOfBoundsException => " + eIOB.getMessage());
+//        }
+        // Create a short form for the catch block exceptions.
+        catch(NumberFormatException | IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
         }
     }
 
