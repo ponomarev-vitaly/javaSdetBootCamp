1a389d19db366a394fd9ae85890e0716aee70aa9
ponomarev-vitaly <vp211082@gmail.com>
2023-03-25 05:15:28 +0300

    I added some comments to try catch blocks.

--- src/week7/TryCatch.java
+++ src/week7/TryCatch.java
@@ -9,8 +9,10 @@ public class TryCatch {
         try{
             file = new FileInputStream("c:/textfile.txt");
         }
-        catch(IOException e){
+        catch(IOException e){ // Catch block can run, if there is an exception.
             System.out.println("The specified file is not present at the given path. " + e);
+        } finally { // Finally code block runs every time.
+            System.out.println("Finally block runs every time.");
         }
     }
 }
