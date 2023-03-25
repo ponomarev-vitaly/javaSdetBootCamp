d338f330fb9e011d9553f8aad5a499f90807577c
ponomarev-vitaly <vp211082@gmail.com>
2023-03-25 22:08:14 +0300

    I added code to catch IOException and provide the message into the console. I also used BufferedReader class object to write messages into the OutputFile.txt.

--- src/week7/Reader.java
+++ src/week7/Reader.java
@@ -1,5 +1,19 @@
 package week7;
 
-public class Reader {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Reader {
+    public static void main(String[] args) {
+        String line;
+        try(BufferedReader br = new BufferedReader(new FileReader("C:\\Java\\ClaruswayProject\\src\\week7\\OutputFile.txt"))){
+            while((line = br.readLine()) != null){
+                System.out.println("Line => " + line);
+            }
+        }catch(IOException e){
+            System.out.println("IOException in try block " + e.getMessage());
+        }
+
+    }
 }
