872296397508e169dd212114ea61326e4cf5e55f
ponomarev-vitaly <vp211082@gmail.com>
2023-03-25 06:15:32 +0300

    I added some useful comments.

--- src/week7/SystemCrash.java
+++ src/week7/SystemCrash.java
@@ -2,8 +2,10 @@ package week7;
 
 public class SystemCrash {
     public static void main(String[] args) {
+        // This is the example of System Crash Error.
         for(int i = 0; i < 3; i--){
             System.out.println(i);
+        // We added if statement to solve this infinite loop.
             if(i == -5) i=4;
         }
     }
