3a1538ab19e4b737c958379d93d53203f13ba6ff
ponomarev-vitaly <vp211082@gmail.com>
2023-03-29 02:55:24 +0300

    I added new class to check the question 43 for java certification test.

---
+++ src/week8/TestQuestion43.java
@@ -0,0 +1,20 @@
+package week8;
+
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//import static jdk.internal.org.jline.utils.Colors.s;
+
+/**
+ * Test question 43.
+ */
+//public class TestQuestion43 {
+//    public static void main(String[] args) {
+//        String[] arr = {"Hi", "How", "Are", "You"};
+//        List<String> arrList = new ArrayList<>(Arrays.asList(arr));
+//        if (arrList.removeIf((String s) -> (return s.length() <= 2;))) {
+//            System.out.println(s + "removed");
+//        }
+//    }
+//}
