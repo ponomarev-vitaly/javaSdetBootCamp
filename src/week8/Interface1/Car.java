d1f6d218e6150f28f362472efb32a4078488d823
ponomarev-vitaly <vp211082@gmail.com>
2023-03-30 06:01:36 +0300

    I 1 abstract method in the car class and 2 concrete methods.

--- src/week8/Interface1/Car.java
+++ src/week8/Interface1/Car.java
@@ -1,4 +1,15 @@
 package week8.Interface1;
 
-public class Car {
+public abstract class Car {
+    // Created abstract method -> without body.
+    public abstract void move();
+
+    // Created concrete methods -> with body.
+    public void diesel(){
+        System.out.println("This car works with diesel.");
+    }
+
+    public void gas(){
+        System.out.println("This car works with cas.");
+    }
 }
