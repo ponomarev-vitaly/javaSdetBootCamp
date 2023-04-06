d25ff7781ae60b17f490c11446be4333f32b9c94
ponomarev-vitaly <vp211082@gmail.com>
2023-04-06 04:51:15 +0300

    I added several methods to apply to lists and demonstrate how these methods work with lists.

--- src/week9/MethodsOfList/MethodsInAction.java
+++ src/week9/MethodsOfList/MethodsInAction.java
@@ -1,6 +1,7 @@
 package week9.MethodsOfList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -29,8 +30,36 @@ public class MethodsInAction {
         4 -> Hello
          */
 
+        Object newList = ((LinkedList<String>) list).clone(); // clone() clones the list. newList is an Object, it's one body.
+        // list.clear(); // This code removes totally the list.
+
+        System.out.println("The List: ");
         for (String str : list) {
             System.out.println(str);
         }
+
+        System.out.println("");
+        String check = "Bye";
+
+        // System.out.println(check + " is available at the index " + list.indexOf(check));
+
+        if(list.contains(check)){
+            System.out.println(check + " is available at the index " + list.indexOf(check));
+        }else{
+            System.out.println(check + " is not available in the list ");
+        }
+
+        System.out.println("");
+
+        System.out.println(list.contains("Bye"));
+        System.out.println(list.get(3)); // list[3] this is how it worked in the arrays.
+
+        System.out.println("The New List: " + newList);
+
+//        List<Object> newest = Arrays.asList(newList);
+//        System.out.println("The Newest: ");
+//        for (Object str : newest) {
+//            System.out.println(str.toString());
+//        }
     }
 }
