f800c9c63ef36711eda5855eb21f0b480a16321b
ponomarev-vitaly <vp211082@gmail.com>
2023-04-08 06:40:57 +0300

    I added more methods to manipulate with the deque.

--- src/week9/DequeExample.java
+++ src/week9/DequeExample.java
@@ -1,6 +1,7 @@
 package week9;
 
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class DequeExample {
@@ -17,6 +18,37 @@ public class DequeExample {
 
         System.out.println(deque + "\n");
 
+        // Iterate through the queue.
+        System.out.println("Standard Iterator");
+        Iterator iterator = deque.iterator();
+        while(iterator.hasNext()) {
+            System.out.println("\t" + iterator.next());
+        }
+        System.out.println();
+        System.out.println("Reverse Iterator");
+        // Reverse order iterator. Create an object from the iterator.
+        Iterator reverse = deque.descendingIterator();
+        while(reverse.hasNext()) {
+            System.out.println("\t" + reverse.next());
+        }
 
+        // peek() method applied.
+        System.out.println("peek() method applied: " + deque.peek());
+        System.out.println("After peek(): " + deque);
+
+        // pop() method applied.
+        System.out.println("pop() method applied: " + deque.pop());
+        System.out.println("After pop(): " + deque);
+
+        // contains() method applied.
+        System.out.println(deque.contains("Element-1 (Tail)"));
+
+        //
+
+        deque.removeFirst();
+        deque.removeLast();
+
+        System.out.println("Deque after removing the first and the last element: " + deque);
     }
 }
+
