44c413aa5c061c43d4e5253873de40bf8a848381
ponomarev-vitaly <vp211082@gmail.com>
2023-04-11 06:17:29 +0300

    I created new class to check others LinkedLists' methods.

---
+++ src/week10/LinkedListExample3.java
@@ -0,0 +1,10 @@
+package week10;
+
+import java.util.LinkedList;
+
+public class LinkedListExample3 {
+    public static void main(String[] args) {
+        LinkedList<String> c1 = new LinkedList<>();
+
+    }
+}
