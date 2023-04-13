d0c69e5564bddaa8f8bf390b83e276409a653cd1
ponomarev-vitaly <vp211082@gmail.com>
2023-04-13 22:21:45 +0300

    I added comment, practiced with the code.

--- src/week10/MapInMap/NestedMap.java
+++ src/week10/MapInMap/NestedMap.java
@@ -7,6 +7,12 @@ import java.util.Map;
 public class NestedMap {
     public static void main(String[] args) {
         HashMap<String, LinkedHashMap<String, Integer>> nested = new HashMap<>();
+//        HashMap<String, LinkedHashMap<String, LinkedHashMap<String, Integer>>> nested1 = new HashMap<>();
+//
+//        nested1.put("Shopping", new LinkedHashMap<>().put("Supermarket", ));
+//        nested1.put("Shopping", (LinkedHashMap<String, LinkedHashMap<String, Integer>>) new LinkedHashMap<>().put("Supermarket", new LinkedHashMap<>()));
+//        nested1.put("ToDo", );
+//        nested1.put("Activities", );
 
         nested.put("Shopping", new LinkedHashMap<>());
         nested.put("ToDo", new LinkedHashMap<>());
@@ -25,6 +31,7 @@ public class NestedMap {
         nested.get("Activities").put("Photography", 1);
         nested.get("Activities").put("Gym", 5);
 
+        // This code is to show data from HashMap.
         for(Map.Entry<String, LinkedHashMap<String, Integer>> outer : nested.entrySet()){
             System.out.println("-----" + outer.getKey() + "-----");
             for(Map.Entry<String, Integer> inner : outer.getValue().entrySet()){
