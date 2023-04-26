a71b1ef582cd5cd98654f58c61cb6067789d44b0
ponomarev-vitaly <vp211082@gmail.com>
2023-04-26 14:46:17 +0300

    This is just a simple recap an example of helloworld console program.

---
+++ src/week1/FirstProgram.java
@@ -0,0 +1,8 @@
+package week1;
+
+public class FirstProgram { // public, class are reserved keywords
+    public static void main(String[] args) { // write main method, because without main method program not going to run
+        System.out.println("Hello geeks!"); // pay attention to the first capital letter, because Java is case-sensitive language
+
+    }
+}
