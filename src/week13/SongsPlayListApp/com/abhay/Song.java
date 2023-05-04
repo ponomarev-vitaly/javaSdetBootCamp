d3759121b830ee71f5839c1f0a642c27f02d3259
ponomarev-vitaly <vp211082@gmail.com>
2023-05-04 21:36:52 +0300

    I created constructor inside the Song class.

---
+++ src/week13/SongsPlayListApp/com/abhay/Song.java
@@ -0,0 +1,11 @@
+package week13.SongsPlayListApp.com.abhay;
+
+public class Song {
+    String name;
+    double duration;
+// Create a constructor.
+    public Song(String name, double duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+}
