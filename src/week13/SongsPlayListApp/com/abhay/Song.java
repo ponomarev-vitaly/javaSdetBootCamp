90674b17dcae42f0f517fe4fd67451d6feea1430
ponomarev-vitaly <vp211082@gmail.com>
2023-05-04 21:39:51 +0300

    I created default constructo and toString() method. I also added getter method.

--- src/week13/SongsPlayListApp/com/abhay/Song.java
+++ src/week13/SongsPlayListApp/com/abhay/Song.java
@@ -1,11 +1,32 @@
 package week13.SongsPlayListApp.com.abhay;
 
 public class Song {
-    String name;
+    String title;
     double duration;
 // Create a constructor.
-    public Song(String name, double duration) {
-        this.name = name;
+    public Song(String title, double duration) {
+        this.title = title;
         this.duration = duration;
+    }
+
+    //
+    public Song(){
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
     }
 }
