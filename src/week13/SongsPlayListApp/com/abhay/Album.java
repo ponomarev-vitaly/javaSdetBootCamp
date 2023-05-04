f5de74231f77d4cfdfd4eaf624a61c4c733e9cce
ponomarev-vitaly <vp211082@gmail.com>
2023-05-05 01:45:19 +0300

    I added new methods in the Album class: addSong(), addToPlayList(), addToPlayList().

--- src/week13/SongsPlayListApp/com/abhay/Album.java
+++ src/week13/SongsPlayListApp/com/abhay/Album.java
@@ -1,6 +1,7 @@
 package week13.SongsPlayListApp.com.abhay;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Album {
     private String name;
@@ -17,9 +18,42 @@ public class Album {
 
     }
 
+    public Song findSong(String title){
+        for(Song chekcedSong : songs){
+            if(chekcedSong.getTitle().equals(title)) return chekcedSong;
+        }
+        return null;
+    }
+
     public boolean addSong(String title, double duration){
         if(findSong(title) == null){
-
+            songs.add(new Song(title, duration));
+            System.out.println(title + "successfully added to the list");
+            return true;
+        } else{
+            System.out.println("Song with name " + title + " already exists in the list");
+            return false;
         }
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> PlayList){
+        int index = trackNumber -1;
+        if(index > 0 && index <= this.songs.size()){
+            PlayList.add(this.songs.get(index));
+            return true;
+        }
+        System.out.println("This album does not have song with tracknumber " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> PlayList){
+        for(Song checkedSong : this.songs){
+            if (checkedSong.getTitle().equals(title)){
+                PlayList.add(checkedSong);
+                return true;
+            }
+        }
+        System.out.println(title + " there is no such song in the album!");
+        return false;
     }
 }
