f4bdf6742a83bb0e325907729fb4126c269c82d1
ponomarev-vitaly <vp211082@gmail.com>
2023-05-05 12:31:52 +0300

    I modified the code.

--- src/week13/SongsPlayListApp/com/abhay/Album.java
+++ src/week13/SongsPlayListApp/com/abhay/Album.java
@@ -8,10 +8,10 @@ public class Album {
     private String artist;
     private ArrayList<Song> songs;
 
-    public Album(String name, String artist, ArrayList<Song> songs) {
+    public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
-        this.songs = new ArrayList<Song>;
+        this.songs = new ArrayList<Song>();
     }
 
     public Album(){
