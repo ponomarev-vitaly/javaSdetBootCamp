8e422034b1900cc86fa15e80074f7047ca399f9f
ponomarev-vitaly <vp211082@gmail.com>
2023-05-05 22:04:43 +0300

    I commented some print methods in Album class. I checked the code and modified the code.

--- src/week13/SongsPlayListApp/com/abhay/Album.java
+++ src/week13/SongsPlayListApp/com/abhay/Album.java
@@ -28,10 +28,10 @@ public class Album {
     public boolean addSong(String title, double duration){
         if(findSong(title) == null){
             songs.add(new Song(title, duration));
-            System.out.println(title + "successfully added to the list");
+//            System.out.println(title + "successfully added to the list");
             return true;
         } else{
-            System.out.println("Song with name " + title + " already exists in the list");
+//            System.out.println("Song with name " + title + " already exists in the list");
             return false;
         }
     }
@@ -42,7 +42,7 @@ public class Album {
             PlayList.add(this.songs.get(index));
             return true;
         }
-        System.out.println("This album does not have song with tracknumber " + trackNumber);
+//        System.out.println("This album does not have song with tracknumber " + trackNumber);
         return false;
     }
 
@@ -53,7 +53,7 @@ public class Album {
                 return true;
             }
         }
-        System.out.println(title + " there is no such song in the album!");
+//        System.out.println(title + " there is no such song in the album!");
         return false;
     }
 }
--- src/week13/SongsPlayListApp/com/abhay/Main.java
+++ src/week13/SongsPlayListApp/com/abhay/Main.java
@@ -25,8 +25,8 @@ public class Main {
 
         albums.get(0).addToPlayList("TNT", playlist_1);
         albums.get(0).addToPlayList("Highway to hellT", playlist_1);
-        albums.get(0).addToPlayList("Rap god", playlist_1);
-        albums.get(0).addToPlayList("Lose yourself", playlist_1);
+        albums.get(1).addToPlayList("Rap god", playlist_1);
+        albums.get(1).addToPlayList("Lose yourself", playlist_1);
 
         play(playlist_1);
     }
@@ -37,6 +37,96 @@ public class Main {
         boolean forward = true;
         ListIterator<Song> listIterator = playList.listIterator();
 
+        if(playList.size() == 0){
+            System.out.println("This playlist has no song!");
+        } else{
+            System.out.println("Now playing " + listIterator.next().toString());
+            printMenu();
+        }
+
+        while(!quit){
+            int action = scan.nextInt();
+            scan.nextLine();
+
+            switch (action){
+
+                case 0:
+                    System.out.println("Playlist complete");
+                    quit = true;
+                    break;
+
+                case 1:
+                    if(!forward){
+                        if(listIterator.hasNext()){
+                            listIterator.next();
+                        }
+                        forward = true;
+                    }
+
+                    if(listIterator.hasNext()){
+                        System.out.println("Now playing " + listIterator.next().toString());
+                    }else{
+                        System.out.println("There is no song available, reached to the end of the list!");
+                        forward = false;
+                    }
+                    break;
+
+                case 2:
+                    if(forward){
+                        if (listIterator.hasPrevious()){
+                            listIterator.previous();
+                        }
+                        forward =  false;
+                    }
+
+                    if(listIterator.hasPrevious()){
+                        System.out.println("Now playing " + listIterator.previous().toString());
+                    }else{
+                        System.out.println("We are at the first song!");
+                        forward =  false;
+                    }
+                    break;
+
+                case 3:
+                    if(forward){
+                        if(listIterator.hasPrevious()){
+                            System.out.println("Now playing " + listIterator.previous().toString());
+                            forward = false;
+                        }else{
+                            System.out.println("We are at the beginning of the list!");
+                        }
+                    }else{
+                        if(listIterator.hasNext()){
+                            System.out.println("Now playing " + listIterator.next().toString());
+                            forward = true;
+                        }else{
+                            System.out.println("We have reached to the end of list!");
+                        }
+                    }
+                    break;
+
+                case 4:
+                    printList(playList);
+                    break;
+
+                case 5:
+                    printMenu();
+                    break;
+
+                case 6:
+                    if(playList.size() > 0){
+                        listIterator.remove();
+                        if(listIterator.hasNext()){
+                            System.out.println("Now playing " + listIterator.next().toString());
+                        }
+                        else{
+                            if(listIterator.hasPrevious())
+                            System.out.println("Now playing " + listIterator.previous().toString());
+                        }
+                    }
+                    break;
+            }
+        }
     }
 
     private static void printMenu(){
