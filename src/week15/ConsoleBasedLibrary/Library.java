14a57a342548ceb03e26570ece8ed285dae6b957
Vitaly Ponomarev <vp211082@gmail.com>
2023-05-18 15:30:13 +0300

    I added getter and setter methods.

--- src/week15/ConsoleBasedLibrary/Library.java
+++ src/week15/ConsoleBasedLibrary/Library.java
@@ -13,5 +13,55 @@ public class Library {
         this.author = author;
         this.numberOfPages = numberOfPages;
         this.theDateOfIssue = theDateOfIssue;
+        IDCounter++;
+        this.ID = IDCounter;
+    }
+
+    public static int getIDCounter() {
+        return IDCounter;
+    }
+
+    public static void setIDCounter(int IDCounter) {
+        Library.IDCounter = IDCounter;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getMyBooksBusiness() {
+        return myBooksBusiness;
+    }
+
+    public void setMyBooksBusiness(String myBooksBusiness) {
+        this.myBooksBusiness = myBooksBusiness;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getTheDateOfIssue() {
+        return theDateOfIssue;
+    }
+
+    public void setTheDateOfIssue(String theDateOfIssue) {
+        this.theDateOfIssue = theDateOfIssue;
     }
 }
