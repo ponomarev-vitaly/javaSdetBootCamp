80c975709a1ba1c981c22b2a70873991ac6e1d54
Vitaly Ponomarev <vp211082@gmail.com>
2023-05-18 19:15:29 +0300

    I added new variables: type and isArticle.

--- src/week15/ConsoleBasedLibrary/Science.java
+++ src/week15/ConsoleBasedLibrary/Science.java
@@ -1,9 +1,13 @@
 package week15.ConsoleBasedLibrary;
 
 public class Science extends Library implements ScientificData{
+    private String type;
+    private boolean isArticle;
 
-    public Science(String myBooksBusiness, String author, int numberOfPages, String theDateOfIssue) {
+    public Science(String myBooksBusiness, String author, int numberOfPages, String theDateOfIssue, String type, boolean isArticle) {
         super(myBooksBusiness, author, numberOfPages, theDateOfIssue);
+        this.type = type;
+        this.isArticle = isArticle;
     }
 
     @Override
