bad943b21dfd69a1d5c90a4ee826d09fd6a3b68b
Vitaly Ponomarev <vp211082@gmail.com>
2023-07-07 02:48:57 +0300

    I added Java classes to check the solutions of the Java certification questions.

---
+++ src/week22/TestQuestion34.java
@@ -0,0 +1,25 @@
+package week22;
+
+class Point {
+    int x;
+    int y;
+    void assign(int x, int y) {
+        x = this.x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "Point(" + x + ", " + y + ")";
+    }
+}
+
+public class TestQuestion34 {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        p1.x = 10;
+        p1.y = 20;
+        Point p2 = new Point();
+        p2.assign(p1.x, p1.y);
+        System.out.println(p1.toString() + ";" + p2.toString());
+    }
+}
