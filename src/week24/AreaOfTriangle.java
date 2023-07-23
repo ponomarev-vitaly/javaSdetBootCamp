748fe18ace2cca760d206a5fd4354f288d9a31fc
Vitaly Ponomarev <vp211082@gmail.com>
2023-07-23 19:57:21 +0300

    I also added another possible solution for that task.

--- src/week24/AreaOfTriangle.java
+++ src/week24/AreaOfTriangle.java
@@ -6,21 +6,22 @@ import static java.lang.Math.sqrt;
 
 public class AreaOfTriangle {
     public static void main(String[] args) {
-
         double sideOfTheTriangleA, sideOfTheTriangleB, sideOfTheTriangleC, halfOfThePerimeter, triangleArea;
         Scanner scan = new Scanner(System.in);
 
+        // Solution 1.
+
         try {
             System.out.print("Please, enter A side of a triangle: ");
-            sideOfTheTriangleA = scan.nextInt();
+            sideOfTheTriangleA = scan.nextDouble();
 
             System.out.print("Please, enter B side of a triangle: ");
-            sideOfTheTriangleB = scan.nextInt();
+            sideOfTheTriangleB = scan.nextDouble();
 
             System.out.print("Please, enter C side of a triangle: ");
-            sideOfTheTriangleC = scan.nextInt();
+            sideOfTheTriangleC = scan.nextDouble();
 
-            if (sideOfTheTriangleA==1 || sideOfTheTriangleB== 1 || sideOfTheTriangleC==1) {
+            if (sideOfTheTriangleA==0 || sideOfTheTriangleB==0 || sideOfTheTriangleC==0) {
                 System.out.println("NaN");
             } else {
                 halfOfThePerimeter = (sideOfTheTriangleA + sideOfTheTriangleB + sideOfTheTriangleC)/2;
@@ -34,5 +35,23 @@ public class AreaOfTriangle {
         }   catch (RuntimeException e){
             throw new RuntimeException();
         }
+
+        // Solution 2.
+
+//        System.out.print("Please, enter A side of a triangle: ");
+//        sideOfTheTriangleA = scan.nextDouble();
+//
+//        System.out.print("Please, enter B side of a triangle: ");
+//        sideOfTheTriangleB = scan.nextDouble();
+//
+//        System.out.print("Please, enter C side of a triangle: ");
+//        sideOfTheTriangleC = scan.nextDouble();
+//
+//        halfOfThePerimeter = (sideOfTheTriangleA + sideOfTheTriangleB + sideOfTheTriangleC)/2;
+//        triangleArea = Math.sqrt(halfOfThePerimeter*(halfOfThePerimeter-sideOfTheTriangleA)*(halfOfThePerimeter-sideOfTheTriangleB)*(halfOfThePerimeter-sideOfTheTriangleC));
+//
+//        System.out.println("Triangle area is: " + triangleArea);
+
+
     }
 }
