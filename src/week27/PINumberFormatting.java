c23de029c233a86fbbc82c9743d06c0d540d53c1
Vitaly Ponomarev <vp211082@gmail.com>
2023-07-25 00:58:24 +0300

    I added new java class PINumberFormatting.

---
+++ src/week27/PINumberFormatting.java
@@ -0,0 +1,11 @@
+package week27;
+
+public class PINumberFormatting{
+    public static void main(String[] args) {
+        System.out.printf("%.1f\n", Math.PI);
+        System.out.printf("%.2f\n", Math.PI);
+        System.out.printf("%.3f\n", Math.PI);
+        System.out.printf("%.4f\n", Math.PI);
+        System.out.printf("%.5f\n", Math.PI);
+    }
+}
