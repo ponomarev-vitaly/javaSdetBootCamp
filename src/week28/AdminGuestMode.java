31591e5314b14945cde1beff9a44c50258f38cfc
ponomarev-vitaly <vp211082@gmail.com>
2023-08-18 23:39:53 +0300

    I added code for 2 modes admin and guest

--- src/week28/AdminGuestMode.java
+++ src/week28/AdminGuestMode.java
@@ -1,4 +1,18 @@
 package week28;
 
+import java.util.Scanner;
+
 public class AdminGuestMode {
+    public static void main(String[] args) {
+        String inputMode;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("This is the main menu. Please enter your choice: for admin mode --admin; for guest mode --guest.");
+        System.out.print("Your choice print hear: ");
+        inputMode = scan.nextLine();
+        switch(inputMode){
+            case "--admin" -> System.out.println("Hello, Admin!");
+            case "--guest" -> System.out.println("Hello, Guest!");
+            default -> System.out.println("Please, select either 'ADMIN' or 'GUEST' mode for this program");
+        }
+    }
 }
