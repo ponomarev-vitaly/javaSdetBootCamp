d77024985bed76095f7b38c42f0d36b2b575baf7
ponomarev-vitaly <vp211082@gmail.com>
2023-08-19 14:36:31 +0300

    I created some code for generating random numbers as a simple example.

--- src/week28/RandomNumbersGenerator.java
+++ src/week28/RandomNumbersGenerator.java
@@ -1,4 +1,12 @@
 package week28;
 
+import java.util.Random;
+
 public class RandomNumbersGenerator {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt());		// any random int
+        System.out.println(r.nextInt(100)); 	// any int from 0 to 99
+        System.out.println(r.nextInt(100) + 1);	// any int from 1 to 100
+    }
 }
