ae00cf45a4ec89bdf0218af84ed0b195307dc706
ponomarev-vitaly <vp211082@gmail.com>
2023-08-19 14:33:31 +0300

    I created code for user inputs and printing some text into console

--- src/week28/UserActivitySelector.java
+++ src/week28/UserActivitySelector.java
@@ -1,5 +1,7 @@
 package week28;
 
+import java.util.Scanner;
+
 /**
  * Implement console program which will meet the following requirements:
  * Program is started and user is asked to enter one of possible events:
@@ -25,4 +27,25 @@ package week28;
  * Please, enter one of these values: login, sign_up, terminate_program, main_menu, about_app
  */
 public class UserActivitySelector {
+    public static void main(String[] args) {
+        System.out.println("<==================== Main menu ====================>");
+        System.out.println("<==================== Please enter one of possible events: ====================>");
+        System.out.println("<==================== login ====================>");
+        System.out.println("<==================== sign_up ====================>");
+        System.out.println("<==================== terminate_program ====================>");
+        System.out.println("<==================== main_menu ====================>");
+        System.out.println("<==================== about_app ====================>");
+        System.out.print("Please, enter action name: ");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+
+        switch(userInput){
+            case "login" -> System.out.println("Please, enter your username");
+            case "sign_up" -> System.out.println("Welcome!");
+            case "terminate_program" -> System.out.println("Thank you! Good bye!");
+            case "main_menu" -> System.out.println("Main menu");
+            case "about_app" -> System.out.println("This app is created by me with support of ®IT-Bulls.com");
+            default -> System.out.println("Please, enter one of these values: login, sign_up, terminate_program, main_menu, about_app");
+        }
+    }
 }
