99ace3de3d17fdfae820630bd8f4bba894378709
ponomarev-vitaly <vp211082@gmail.com>
2023-08-19 14:15:39 +0300

    I added new Java class UserActivitySelector

---
+++ src/week28/UserActivitySelector.java
@@ -0,0 +1,28 @@
+package week28;
+
+/**
+ * Implement console program which will meet the following requirements:
+ * Program is started and user is asked to enter one of possible events:
+ * login
+ * sign_up
+ * terminate_program
+ * main_menu
+ * about_app
+ * Based on user input program prints to console different text. Mapping is below:
+ * User Input
+ * Console output
+ * login
+ * Please, enter your username
+ * sign_up
+ * Welcome!
+ * terminate_program
+ * Thank you! Good bye!
+ * main_menu
+ * Main menu
+ * about_app
+ * This app is created by me with support of ®IT-Bulls.com
+ * <any other input>
+ * Please, enter one of these values: login, sign_up, terminate_program, main_menu, about_app
+ */
+public class UserActivitySelector {
+}
